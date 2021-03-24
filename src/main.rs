3b7e231031b4300e6b1efc83e5f711ff74cd4026
Evan Relf <evan@evanrelf.com>
2021-03-23 18:04:26 -0700

    Create basic event loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use crossterm::{cursor, style, terminal, ExecutableCommand, Result};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers, MouseEvent};
+use crossterm::{cursor, event, style, terminal, ExecutableCommand, Result};
 use std::io::stdout;
-use std::thread::sleep;
-use std::time::Duration;
 
 struct Terminal {}
 
@@ -35,6 +34,47 @@ impl Terminal {
     }
 }
 
+struct Editor {
+    quit: bool,
+}
+
+impl Editor {
+    fn new() -> Editor {
+        Editor { quit: false }
+    }
+
+    fn handle_event(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) => self.handle_key_event(key_event),
+            Event::Mouse(mouse_event) => self.handle_mouse_event(mouse_event),
+            Event::Resize(width, height) => self.handle_resize_event(width, height),
+        }
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
+        let KeyEvent { code, modifiers } = key_event;
+
+        if modifiers == KeyModifiers::CONTROL {
+            match code {
+                KeyCode::Char('c') => self.quit = true,
+                _ => (),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_mouse_event(&mut self, mouse_event: MouseEvent) -> Result<()> {
+        match mouse_event {
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_resize_event(&mut self, _width: u16, _height: u16) -> Result<()> {
+        Ok(())
+    }
+}
+
 fn main() -> Result<()> {
     Terminal::enter();
 
@@ -42,9 +82,12 @@ fn main() -> Result<()> {
 
     Terminal::clear();
     Terminal::move_cursor_to(0, 0);
-    Terminal::print("Hello, world!");
 
-    sleep(Duration::new(1, 0));
+    let mut editor = Editor::new();
+
+    while !editor.quit {
+        editor.handle_event()?;
+    }
 
     Terminal::exit();
 
