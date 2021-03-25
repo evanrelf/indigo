2be14f6177fcd1de49d3bd4f54eb71201096d4be
Evan Relf <evan@evanrelf.com>
2021-03-24 17:38:13 -0700

    Separate handling inputs from performing actions

--- src/editor.rs
+++ src/editor.rs
@@ -4,6 +4,30 @@ use crossterm::Result;
 use ropey::Rope;
 use std::borrow::Cow;
 
+pub enum Action {
+    Quit,
+    InsertChar(usize, usize, char),
+    DeleteChar(usize, usize),
+}
+
+impl Action {
+    pub fn apply(self, editor: &mut Editor) {
+        match self {
+            Action::Quit => {
+                editor.quit = true;
+            }
+            Action::InsertChar(line, column, c) => {
+                let index = editor.buffer.line_to_char(line) + column;
+                editor.buffer.insert_char(index, c);
+            }
+            Action::DeleteChar(line, column) => {
+                let index = editor.buffer.line_to_char(line) + column;
+                editor.buffer.remove(index..(index + 1));
+            }
+        }
+    }
+}
+
 pub struct Editor {
     pub quit: bool,
     pub title: String,
@@ -26,13 +50,15 @@ impl Editor {
 
         while !self.quit {
             self.render();
-            self.handle_event().unwrap();
+            if let Some(action) = self.handle_event().unwrap() {
+                action.apply(self);
+            }
         }
 
         Terminal::exit();
     }
 
-    pub fn handle_event(&mut self) -> Result<()> {
+    pub fn handle_event(&mut self) -> Result<Option<Action>> {
         match event::read()? {
             Event::Key(key_event) => self.handle_key_event(key_event),
             Event::Mouse(mouse_event) => self.handle_mouse_event(mouse_event),
@@ -40,45 +66,43 @@ impl Editor {
         }
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<Option<Action>> {
         let KeyEvent { code, modifiers } = key_event;
 
         if modifiers == KeyModifiers::NONE {
             let length = self.buffer.len_chars();
-            match code {
-                KeyCode::Char(c) => {
-                    self.buffer.insert_char(length, c);
-                }
-                KeyCode::Enter => {
-                    self.buffer.insert(length, "\n");
-                }
+            return Ok(match code {
+                KeyCode::Char(c) => Some(Action::InsertChar(0, length, c)),
+                KeyCode::Enter => Some(Action::InsertChar(0, length, '\n')),
                 KeyCode::Backspace => {
                     if length > 0 {
-                        self.buffer.remove((length - 1)..length);
+                        Some(Action::DeleteChar(0, length - 1))
+                    } else {
+                        None
                     }
                 }
-                _ => (),
-            }
+                _ => None,
+            });
         }
 
         if modifiers == KeyModifiers::CONTROL {
-            match code {
-                KeyCode::Char('c') => self.quit = true,
-                _ => (),
-            }
+            return Ok(match code {
+                KeyCode::Char('c') => Some(Action::Quit),
+                _ => None,
+            });
         }
 
-        Ok(())
+        Ok(None)
     }
 
-    fn handle_mouse_event(&mut self, mouse_event: MouseEvent) -> Result<()> {
-        match mouse_event {
-            _ => Ok(()),
-        }
+    fn handle_mouse_event(&mut self, mouse_event: MouseEvent) -> Result<Option<Action>> {
+        Ok(match mouse_event {
+            _ => None,
+        })
     }
 
-    fn handle_resize_event(&mut self, _width: u16, _height: u16) -> Result<()> {
-        Ok(())
+    fn handle_resize_event(&mut self, _width: u16, _height: u16) -> Result<Option<Action>> {
+        Ok(None)
     }
 
     pub fn render(&mut self) {
