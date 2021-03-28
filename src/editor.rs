4aa89f810400c014e52f78ab410fdc8a9247202a
Evan Relf <evan@evanrelf.com>
2021-03-28 16:02:55 -0700

    Add support for multiple selections to types

--- src/editor.rs
+++ src/editor.rs
@@ -21,10 +21,10 @@ pub enum Action {
     ChangeMode(Mode),
     InsertChar(usize, usize, char),
     DeleteChar(usize, usize),
-    MoveCursor(Direction, usize),
-    MoveAnchor(Direction, usize),
-    ReduceSelection,
-    FlipSelection,
+    MoveCursor(usize, Direction, usize),
+    MoveAnchor(usize, Direction, usize),
+    ReduceSelection(usize),
+    FlipSelection(usize),
 }
 
 #[derive(Debug)]
@@ -37,7 +37,7 @@ pub struct Editor {
     pub quit: bool,
     pub title: String,
     pub mode: Mode,
-    pub selection: Selection,
+    pub selections: Vec<Selection>,
     pub buffer: Buffer,
 }
 
@@ -47,7 +47,7 @@ impl Editor {
             quit: false,
             title: String::from("ind"),
             mode: Mode::Normal,
-            selection: Selection::new(),
+            selections: vec![Selection::new()],
             buffer: Buffer::new(Rope::new()),
         }
     }
@@ -93,36 +93,36 @@ impl Editor {
             match code {
                 KeyCode::Char('i') => Some(vec![Action::ChangeMode(Mode::Insert)]),
                 KeyCode::Char('h') => Some(vec![
-                    Action::MoveCursor(Direction::Left, 1),
-                    Action::ReduceSelection,
+                    Action::MoveCursor(0, Direction::Left, 1),
+                    Action::ReduceSelection(0),
                 ]),
                 KeyCode::Char('j') => Some(vec![
-                    Action::MoveCursor(Direction::Down, 1),
-                    Action::ReduceSelection,
+                    Action::MoveCursor(0, Direction::Down, 1),
+                    Action::ReduceSelection(0),
                 ]),
                 KeyCode::Char('k') => Some(vec![
-                    Action::MoveCursor(Direction::Up, 1),
-                    Action::ReduceSelection,
+                    Action::MoveCursor(0, Direction::Up, 1),
+                    Action::ReduceSelection(0),
                 ]),
                 KeyCode::Char('l') => Some(vec![
-                    Action::MoveCursor(Direction::Right, 1),
-                    Action::ReduceSelection,
+                    Action::MoveCursor(0, Direction::Right, 1),
+                    Action::ReduceSelection(0),
                 ]),
                 _ => None,
             }
         } else if modifiers == KeyModifiers::SHIFT {
             match code {
-                KeyCode::Char('H') => Some(vec![Action::MoveCursor(Direction::Left, 1)]),
-                KeyCode::Char('J') => Some(vec![Action::MoveCursor(Direction::Down, 1)]),
-                KeyCode::Char('K') => Some(vec![Action::MoveCursor(Direction::Up, 1)]),
-                KeyCode::Char('L') => Some(vec![Action::MoveCursor(Direction::Right, 1)]),
+                KeyCode::Char('H') => Some(vec![Action::MoveCursor(0, Direction::Left, 1)]),
+                KeyCode::Char('J') => Some(vec![Action::MoveCursor(0, Direction::Down, 1)]),
+                KeyCode::Char('K') => Some(vec![Action::MoveCursor(0, Direction::Up, 1)]),
+                KeyCode::Char('L') => Some(vec![Action::MoveCursor(0, Direction::Right, 1)]),
                 _ => None,
             }
         } else if modifiers == KeyModifiers::CONTROL {
             None
         } else if modifiers == KeyModifiers::ALT {
             match code {
-                KeyCode::Char(';') => Some(vec![Action::FlipSelection]),
+                KeyCode::Char(';') => Some(vec![Action::FlipSelection(0)]),
                 _ => None,
             }
         } else {
@@ -132,23 +132,23 @@ impl Editor {
 
     fn handle_key_event_insert_mode(&self, key_event: KeyEvent) -> Option<Vec<Action>> {
         let KeyEvent { code, .. } = key_event;
-        let cursor = &self.selection.cursor;
+        let cursor = &self.selections[0].cursor;
         match code {
             KeyCode::Esc => Some(vec![Action::ChangeMode(Mode::Normal)]),
             KeyCode::Char(c) => Some(vec![
                 Action::InsertChar(cursor.line, cursor.column, c),
-                Action::MoveCursor(Direction::Right, 1),
-                Action::ReduceSelection,
+                Action::MoveCursor(0, Direction::Right, 1),
+                Action::ReduceSelection(0),
             ]),
             KeyCode::Enter => Some(vec![
                 Action::InsertChar(cursor.line, cursor.column, '\n'),
-                Action::MoveCursor(Direction::Down, 1),
-                Action::ReduceSelection,
+                Action::MoveCursor(0, Direction::Down, 1),
+                Action::ReduceSelection(0),
             ]),
             KeyCode::Backspace if cursor.column > 0 => Some(vec![
                 Action::DeleteChar(cursor.line, cursor.column - 1),
-                Action::MoveCursor(Direction::Left, 1),
-                Action::ReduceSelection,
+                Action::MoveCursor(1, Direction::Left, 1),
+                Action::ReduceSelection(0),
             ]),
             _ => None,
         }
@@ -184,34 +184,34 @@ impl Editor {
                 self.buffer.insert_char(line, column, character)
             }
             Action::DeleteChar(line, column) => self.buffer.delete_char(line, column),
-            Action::MoveCursor(direction, distance) => {
+            Action::MoveCursor(index, direction, distance) => {
                 match direction {
-                    Direction::Up => self.selection.cursor.move_up(distance),
-                    Direction::Down => self.selection.cursor.move_down(distance),
-                    Direction::Left => self.selection.cursor.move_left(distance),
-                    Direction::Right => self.selection.cursor.move_right(distance),
+                    Direction::Up => self.selections[index].cursor.move_up(distance),
+                    Direction::Down => self.selections[index].cursor.move_down(distance),
+                    Direction::Left => self.selections[index].cursor.move_left(distance),
+                    Direction::Right => self.selections[index].cursor.move_right(distance),
                 };
             }
-            Action::MoveAnchor(direction, distance) => {
+            Action::MoveAnchor(index, direction, distance) => {
                 match direction {
-                    Direction::Up => self.selection.anchor.move_up(distance),
-                    Direction::Down => self.selection.anchor.move_down(distance),
-                    Direction::Left => self.selection.anchor.move_left(distance),
-                    Direction::Right => self.selection.anchor.move_right(distance),
+                    Direction::Up => self.selections[index].anchor.move_up(distance),
+                    Direction::Down => self.selections[index].anchor.move_down(distance),
+                    Direction::Left => self.selections[index].anchor.move_left(distance),
+                    Direction::Right => self.selections[index].anchor.move_right(distance),
                 };
             }
-            Action::ReduceSelection => self.selection.reduce(),
-            Action::FlipSelection => self.selection.flip(),
+            Action::ReduceSelection(index) => self.selections[index].reduce(),
+            Action::FlipSelection(index) => self.selections[index].flip(),
         }
     }
 
-    pub fn cursor_to_char(&self) -> usize {
-        let Position { line, column } = self.selection.cursor;
+    pub fn cursor_to_char(&self, index: usize) -> usize {
+        let Position { line, column } = self.selections[index].cursor;
         self.buffer.contents.line_to_char(line) + column
     }
 
-    pub fn anchor_to_char(&self) -> usize {
-        let Position { line, column } = self.selection.anchor;
+    pub fn anchor_to_char(&self, index: usize) -> usize {
+        let Position { line, column } = self.selections[index].anchor;
         self.buffer.contents.line_to_char(line) + column
     }
 
@@ -219,7 +219,7 @@ impl Editor {
         Terminal::clear();
 
         self.render_buffer();
-        self.render_selection();
+        self.render_selections();
         self.render_status();
 
         Terminal::set_title(&self.title);
@@ -241,17 +241,23 @@ impl Editor {
         }
     }
 
-    pub fn render_selection(&self) {
-        let cursor_line = self.selection.cursor.line;
-        let cursor_column = self.selection.cursor.column;
+    pub fn render_selections(&self) {
+        for index in 0..self.selections.len() {
+            self.render_selection(index);
+        }
+    }
 
-        let anchor_line = self.selection.anchor.line;
-        let anchor_column = self.selection.anchor.column;
+    pub fn render_selection(&self, index: usize) {
+        let cursor_line = self.selections[index].cursor.line;
+        let cursor_column = self.selections[index].cursor.column;
+
+        let anchor_line = self.selections[index].anchor.line;
+        let anchor_column = self.selections[index].anchor.column;
 
         // Anchor
         Terminal::move_cursor_to(anchor_column as u16, anchor_line as u16);
 
-        let anchor_char_index = self.anchor_to_char();
+        let anchor_char_index = self.anchor_to_char(0);
 
         let anchor_char = if self.buffer.contents.len_chars() > anchor_char_index {
             self.buffer.contents.char(anchor_char_index).to_string()
@@ -264,7 +270,7 @@ impl Editor {
         // Cursor
         Terminal::move_cursor_to(cursor_column as u16, cursor_line as u16);
 
-        let cursor_char_index = self.cursor_to_char();
+        let cursor_char_index = self.cursor_to_char(0);
 
         let cursor_char = if self.buffer.contents.len_chars() > cursor_char_index {
             self.buffer.contents.char(cursor_char_index).to_string()
@@ -279,12 +285,12 @@ impl Editor {
         let (_, terminal_lines) = Terminal::size();
         Terminal::move_cursor_to(0, terminal_lines);
         Terminal::print(&format!(
-            "mode: {:?}, cursor: ({}, {}), anchor: ({}, {})",
+            "mode: {:?}, cursor 0: ({}, {}), anchor 0: ({}, {})",
             self.mode,
-            self.selection.cursor.line,
-            self.selection.cursor.column,
-            self.selection.anchor.line,
-            self.selection.anchor.column,
+            self.selections[0].cursor.line,
+            self.selections[0].cursor.column,
+            self.selections[0].anchor.line,
+            self.selections[0].anchor.column,
         ));
     }
 }
