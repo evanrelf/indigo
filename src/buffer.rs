455a46f49750228576cce8d2852b0537a64bd11e
Evan Relf <evan@evanrelf.com>
2021-03-29 16:47:02 -0700

    Create `Selection` and `Buffer` structs

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,1 +1,11 @@
-pub struct Buffer {}
+use ropey::Rope;
+
+pub struct Selection {
+    anchor_index: usize,
+    cursor_index: usize,
+}
+
+pub struct Buffer {
+    contents: Rope,
+    selections: Vec<Selection>,
+}
