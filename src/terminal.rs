5928e205befbdb7821e208a62abb3c2504bdaa48
Evan Relf <evan@evanrelf.com>
2021-03-29 16:22:31 -0700

    Add `Terminal::{enter,exit}` associated functions

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,1 +1,18 @@
+use crossterm::{cursor, terminal, ExecutableCommand, QueueableCommand};
+use std::io::{stdout, Write};
+
 pub struct Terminal {}
+
+impl Terminal {
+    pub fn enter() {
+        terminal::enable_raw_mode().unwrap();
+        stdout().execute(terminal::EnterAlternateScreen).unwrap();
+    }
+
+    pub fn exit() {
+        stdout().queue(cursor::Show).unwrap();
+        stdout().queue(terminal::LeaveAlternateScreen).unwrap();
+        stdout().flush().unwrap();
+        terminal::disable_raw_mode().unwrap();
+    }
+}
