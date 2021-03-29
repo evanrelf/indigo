6ae45ccab44e7a7f8706d26e170329d998f77488
Evan Relf <evan@evanrelf.com>
2021-03-29 16:12:11 -0700

    Start over with new wisdom

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-mod buffer;
-mod editor;
-mod selection;
-mod terminal;
-
-use crate::editor::Editor;
-use crate::terminal::Terminal;
-use crossterm::Result;
-use std::env;
-use std::panic;
-
-fn main() -> Result<()> {
-    panic::set_hook(Box::new(|panic_info| {
-        Terminal::exit();
-        eprintln!("{}", panic_info);
-    }));
-
-    if let Some(path) = env::args().nth(1) {
-        Editor::from_file(path).run();
-    } else {
-        Editor::empty().run();
-    }
-
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
