f53fc9a8240649b5c431f0e88b761141df942a71
Evan Relf <evan@evanrelf.com>
2021-03-29 17:06:40 -0700

    Write `new` for `Editor`

--- src/editor.rs
+++ src/editor.rs
@@ -1,1 +1,15 @@
-pub struct Editor {}
+use crate::buffer::Buffer;
+
+pub struct Editor {
+    quit: bool,
+    buffers: Vec<Buffer>,
+}
+
+impl Editor {
+    pub fn new() -> Editor {
+        Editor {
+            quit: false,
+            buffers: Vec::new(),
+        }
+    }
+}
