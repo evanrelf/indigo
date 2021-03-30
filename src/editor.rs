03883620a602426e162c644bd84d7c91c62d7cc3
Evan Relf <evan@evanrelf.com>
2021-03-29 20:06:10 -0700

    Print Vim-style `~`s along left side

--- src/editor.rs
+++ src/editor.rs
@@ -57,7 +57,12 @@ impl Editor {
     pub fn render(&self) {
         Terminal::queue(terminal::Clear(terminal::ClearType::All));
         Terminal::queue(cursor::MoveTo(0, 0));
-        Terminal::queue(style::Print("Hello, world!"));
+
+        for line in 0..self.viewport.lines {
+            Terminal::queue(style::Print("~"));
+            Terminal::queue(cursor::MoveToNextLine(1));
+        }
+
         Terminal::flush();
     }
 }
