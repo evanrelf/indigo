fc07559adc8496bce9bbfec44f2bd17571163359
Evan Relf <evan@evanrelf.com>
2021-08-31 18:53:09 -0700

    Flesh out selection implementation

--- src/selection.rs
+++ src/selection.rs
@@ -1,8 +1,69 @@
 use crate::position::Position;
+use std::fmt::Display;
 
 pub struct Selection {
-    anchor: Position,
-    cursor: Position,
+    pub anchor: Position,
+    pub cursor: Position,
+}
+
+impl Selection {
+    pub fn new<P>(anchor: P, cursor: P) -> Selection
+    where
+        Position: From<P>,
+    {
+        Selection {
+            anchor: Position::from(anchor),
+            cursor: Position::from(cursor),
+        }
+    }
+
+    pub fn is_forwards(&self) -> bool {
+        self.anchor <= self.cursor
+    }
+
+    pub fn is_backwards(&self) -> bool {
+        self.anchor > self.cursor
+    }
+
+    pub fn is_reduced(&self) -> bool {
+        self.anchor == self.cursor
+    }
+
+    pub fn flip(&mut self) -> &Selection {
+        let anchor = self.anchor;
+        self.anchor = self.cursor;
+        self.cursor = anchor;
+        self
+    }
+
+    pub fn flip_forwards(&mut self) -> &Selection {
+        if self.is_backwards() {
+            self.flip();
+        }
+        self
+    }
+
+    pub fn flip_backwards(&mut self) -> &Selection {
+        if self.is_forwards() {
+            self.flip();
+        }
+        self
+    }
+}
+
+impl Display for Selection {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "{}-{}", self.anchor, self.cursor)
+    }
+}
+
+impl Default for Selection {
+    fn default() -> Selection {
+        Selection {
+            anchor: Position::default(),
+            cursor: Position::default(),
+        }
+    }
 }
 
 impl From<(usize, usize)> for Selection {
@@ -19,15 +80,6 @@ impl From<Position> for Selection {
         Selection {
             anchor: position.clone(),
             cursor: position.clone(),
-        }
-    }
-}
-
-impl Default for Selection {
-    fn default() -> Selection {
-        Selection {
-            anchor: Position::default(),
-            cursor: Position::default(),
         }
     }
 }
