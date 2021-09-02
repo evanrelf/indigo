4cfc14975b9fa93d159ee02687c8d285892e7cd4
Evan Relf <evan@evanrelf.com>
2021-09-02 15:26:47 -0700

    Render selections

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
 use crate::buffer::Buffer;
 use crate::terminal::Terminal;
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::style::Stylize;
 use crossterm::{cursor, event, style, terminal};
 use std::path::Path;
 
@@ -54,6 +55,7 @@ impl Editor {
 
         self.render_tildes();
         self.render_buffer();
+        self.render_selections();
 
         Terminal::flush();
     }
@@ -93,6 +95,56 @@ impl Editor {
                 Terminal::queue(cursor::MoveTo(0, i as u16));
                 Terminal::queue(terminal::Clear(terminal::ClearType::CurrentLine));
                 Terminal::queue(style::Print(String::from(line).trim_end()));
+            }
+        }
+    }
+
+    fn render_selections(&self) {
+        let buffer = &self.buffers[self.buffer_index];
+
+        for selection_mutex in &buffer.selections {
+            let selection = selection_mutex.lock().unwrap();
+
+            let anchor_visible = selection.anchor.line >= buffer.viewport_lines_offset
+                && selection.anchor.column >= buffer.viewport_columns_offset;
+
+            if anchor_visible && !selection.is_reduced() {
+                let anchor_line = (selection.anchor.line - buffer.viewport_lines_offset) as u16;
+                let anchor_column =
+                    (selection.anchor.column - buffer.viewport_columns_offset) as u16;
+                let anchor_char = {
+                    let anchor_index = buffer.position_to_index(&selection.anchor).unwrap();
+                    let char = buffer.contents.char(anchor_index);
+                    if char == '\n' {
+                        ' '
+                    } else {
+                        char
+                    }
+                };
+
+                Terminal::queue(cursor::MoveTo(anchor_column, anchor_line));
+                Terminal::queue(style::PrintStyledContent(anchor_char.on_cyan()));
+            }
+
+            let cursor_visible = selection.cursor.line >= buffer.viewport_lines_offset
+                && selection.cursor.column >= buffer.viewport_columns_offset;
+
+            if cursor_visible {
+                let cursor_line = (selection.cursor.line - buffer.viewport_lines_offset) as u16;
+                let cursor_column =
+                    (selection.cursor.column - buffer.viewport_columns_offset) as u16;
+                let cursor_char = {
+                    let cursor_index = buffer.position_to_index(&selection.cursor).unwrap();
+                    let char = buffer.contents.char(cursor_index);
+                    if char == '\n' {
+                        ' '
+                    } else {
+                        char
+                    }
+                };
+
+                Terminal::queue(cursor::MoveTo(cursor_column, cursor_line));
+                Terminal::queue(style::PrintStyledContent(cursor_char.on_yellow()));
             }
         }
     }
