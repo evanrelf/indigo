37ce3dc05d16e728b7c52c760fc940dbdb866cf6
Evan Relf <evan@evanrelf.com>
2022-03-28 13:35:21 -0700

    Set `RUST_BACKTRACE=1` when in debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use indigo::{editor::Editor, terminal::run};
 
 fn main() {
+    #[cfg(debug_assertions)]
+    std::env::set_var("RUST_BACKTRACE", "1");
+
     let mut editor = Editor::new();
 
     if let Some(path) = std::env::args().nth(1) {
