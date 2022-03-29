d1177512ebccb420c01e047a15c31b08baeff703
Evan Relf <evan@evanrelf.com>
2022-03-28 17:25:39 -0700

    Only set `RUST_BACKTRACE` if not already set

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use indigo::{editor::Editor, terminal::run};
 
 fn main() {
     #[cfg(debug_assertions)]
-    std::env::set_var("RUST_BACKTRACE", "1");
+    if std::env::var("RUST_BACKTRACE").is_err() {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
 
     let mut editor = Editor::new();
 
