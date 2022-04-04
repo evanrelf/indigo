4a4fa6d42154c0db908cf16f642c34ed092d41f3
Evan Relf <evan@evanrelf.com>
2022-04-03 22:38:38 -0700

    Factor out common parts of directional movement

--- src/movement.rs
+++ src/movement.rs
@@ -2,37 +2,44 @@ use crate::{position::Position, range::Range};
 use ropey::Rope;
 use std::cmp::{max, min};
 
+enum Direction {
+    Backward,
+    Forward,
+}
+
 pub fn move_up(range: &Range, rope: &Rope, distance: usize) -> Range {
-    // Prevent `corrected` from moving us to the first index in the rope if
-    // we try to go above the first line
-    let desired_head = Position {
-        line: max(0, range.head.line.saturating_sub(distance)),
-        column: range.target_column.unwrap_or(range.head.column),
-    };
-
-    let (corrected_head, _) = desired_head.corrected(rope);
-
-    let target_column = if corrected_head.column == desired_head.column {
-        None
-    } else {
-        Some(desired_head.column)
-    };
-
-    Range {
-        anchor: range.anchor.clone(),
-        head: corrected_head,
-        target_column,
-    }
+    move_vertically(range, rope, Direction::Backward, distance)
 }
 
 pub fn move_down(range: &Range, rope: &Rope, distance: usize) -> Range {
-    let last_line = rope.len_lines().saturating_sub(2);
+    move_vertically(range, rope, Direction::Forward, distance)
+}
 
-    let desired_head = Position {
-        // Prevent `corrected` from moving us to the last index in the rope
-        // if we try to go below the last line
-        line: min(range.head.line + distance, last_line),
-        column: range.target_column.unwrap_or(range.head.column),
+pub fn move_left(range: &Range, rope: &Rope, distance: usize) -> Range {
+    move_horizontally(range, rope, Direction::Backward, distance)
+}
+
+pub fn move_right(range: &Range, rope: &Rope, distance: usize) -> Range {
+    move_horizontally(range, rope, Direction::Forward, distance)
+}
+
+fn move_vertically(range: &Range, rope: &Rope, direction: Direction, distance: usize) -> Range {
+    let desired_head = match direction {
+        Direction::Backward => Position {
+            // Prevent `corrected` from moving us to the first index in the rope if
+            // we try to go above the first line
+            line: max(0, range.head.line.saturating_sub(distance)),
+            column: range.target_column.unwrap_or(range.head.column),
+        },
+        Direction::Forward => {
+            let last_line = rope.len_lines().saturating_sub(2);
+            Position {
+                // Prevent `corrected` from moving us to the last index in the rope
+                // if we try to go below the last line
+                line: min(range.head.line + distance, last_line),
+                column: range.target_column.unwrap_or(range.head.column),
+            }
+        }
     };
 
     let (corrected_head, _) = desired_head.corrected(rope);
@@ -50,22 +57,17 @@ pub fn move_down(range: &Range, rope: &Rope, distance: usize) -> Range {
     }
 }
 
-pub fn move_left(range: &Range, rope: &Rope, distance: usize) -> Range {
-    let (index, _) = range.head.to_rope_index_lossy(rope);
+fn move_horizontally(range: &Range, rope: &Rope, direction: Direction, distance: usize) -> Range {
+    let index = match direction {
+        Direction::Backward => range
+            .head
+            .to_rope_index_lossy(rope)
+            .0
+            .saturating_sub(distance),
+        Direction::Forward => range.head.to_rope_index_lossy(rope).0 + distance,
+    };
 
-    let (new_head, _) = Position::from_rope_index_lossy(rope, index.saturating_sub(distance));
-
-    Range {
-        anchor: range.anchor.clone(),
-        head: new_head,
-        target_column: None,
-    }
-}
-
-pub fn move_right(range: &Range, rope: &Rope, distance: usize) -> Range {
-    let (index, _) = range.head.to_rope_index_lossy(rope);
-
-    let (new_head, _) = Position::from_rope_index_lossy(rope, index + distance);
+    let (new_head, _) = Position::from_rope_index_lossy(rope, index);
 
     Range {
         anchor: range.anchor.clone(),
