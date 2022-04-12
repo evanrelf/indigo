3127e89ee39848a8c168e9fddcac160b7990553f
Evan Relf <evan@evanrelf.com>
2022-04-11 19:19:36 -0700

    Initial success

--- indigo_state/src/reducer.rs
+++ indigo_state/src/reducer.rs
@@ -1,12 +1,4 @@
-use crate::store::Store;
-use std::{any::TypeId, marker::PhantomData};
-
-// Problems to solve:
-// - Store reducers for calling in the future
-// - Get values from `TypeMap` to give to reducer functions
-// - Dispatch actions to reducers that can handle them
-
-/******************************************************************************/
+use std::marker::PhantomData;
 
 pub trait Reducer {
     type State: 'static;
@@ -14,37 +6,37 @@ pub trait Reducer {
     fn reduce(&self, state: &mut Self::State, action: &Self::Action);
 }
 
-pub trait IntoReducer<State, Action> {
-    type Reducer: Reducer<State = State, Action = Action>;
+pub trait IntoReducer<S, A> {
+    type Reducer: 'static + Reducer<State = S, Action = A>;
     fn into_reducer(self) -> Self::Reducer;
 }
 
-pub struct FunctionReducer<State, Action, F> {
+pub struct FunctionReducer<S, A, F> {
     function: F,
     #[allow(clippy::type_complexity)]
-    marker: PhantomData<fn() -> (State, Action, F)>,
+    marker: PhantomData<fn() -> (S, A, F)>,
 }
 
-impl<State, Action, F> Reducer for FunctionReducer<State, Action, F>
+impl<S, A, F> Reducer for FunctionReducer<S, A, F>
 where
-    State: 'static,
-    Action: 'static,
-    F: Fn(&mut State, &Action),
+    S: 'static,
+    A: 'static,
+    F: Fn(&mut S, &A),
 {
-    type State = State;
-    type Action = Action;
+    type State = S;
+    type Action = A;
     fn reduce(&self, state: &mut Self::State, action: &Self::Action) {
         (self.function)(state, action);
     }
 }
 
-impl<State, Action, F> IntoReducer<State, Action> for F
+impl<S, A, F> IntoReducer<S, A> for F
 where
-    State: 'static,
-    Action: 'static,
-    F: Fn(&mut State, &Action),
+    S: 'static,
+    A: 'static,
+    F: 'static + Fn(&mut S, &A),
 {
-    type Reducer = FunctionReducer<State, Action, F>;
+    type Reducer = FunctionReducer<S, A, F>;
     fn into_reducer(self) -> Self::Reducer {
         FunctionReducer {
             function: self,
@@ -52,37 +44,3 @@ where
         }
     }
 }
-
-#[cfg(test)]
-mod test {
-    #![allow(dead_code)]
-
-    use super::*;
-
-    struct Counter(isize);
-
-    enum CounterAction {
-        Incremented,
-        Decremented,
-    }
-
-    fn counter_reducer(state: &mut Counter, action: &CounterAction) {
-        use CounterAction::*;
-
-        match action {
-            Incremented => state.0 += 1,
-            Decremented => state.0 -= 1,
-        }
-    }
-
-    #[test]
-    fn test() {
-        let mut state = Counter(0);
-        let action = CounterAction::Incremented;
-        let reducer = counter_reducer.into_reducer();
-        reducer.reduce(&mut state, &action);
-        assert_eq!(state.0, 1);
-    }
-}
-
-/******************************************************************************/
--- indigo_state/src/store.rs
+++ indigo_state/src/store.rs
@@ -1,8 +1,10 @@
-use crate::type_map::TypeMap;
+use crate::{reducer::*, type_map::TypeMap};
+use std::any::Any;
 
 #[derive(Default)]
 pub struct Store {
     state: TypeMap,
+    reducers: Vec<Box<dyn StoreReducer>>,
 }
 
 impl Store {
@@ -15,5 +17,110 @@ impl Store {
         S: 'static,
     {
         self.state.insert(state);
+    }
+
+    pub fn get_state<S>(&self) -> Option<&S>
+    where
+        S: 'static,
+    {
+        self.state.get()
+    }
+
+    pub fn add_reducer<S, A, R>(&mut self, reducer: R)
+    where
+        R: IntoReducer<S, A>,
+    {
+        self.reducers.push(Box::new(reducer.into_reducer()));
+    }
+
+    pub fn dispatch<A>(&mut self, action: A)
+    where
+        A: 'static,
+    {
+        for reducer in &self.reducers {
+            reducer.reduce(&mut self.state, &action);
+        }
+    }
+}
+
+pub trait StoreReducer {
+    fn reduce(&self, type_map: &mut TypeMap, action: &dyn Any);
+}
+
+impl<R> StoreReducer for R
+where
+    R: Reducer,
+{
+    fn reduce(&self, type_map: &mut TypeMap, action: &dyn Any) {
+        let state = match type_map.get_mut() {
+            None => panic!("Reducer references state not present in store"),
+            Some(s) => s,
+        };
+        let action = match action.downcast_ref() {
+            None => return,
+            Some(a) => a,
+        };
+        self.reduce(state, action);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #![allow(dead_code)]
+
+    use super::*;
+
+    #[derive(Debug, PartialEq)]
+    struct Name(String);
+
+    enum NameAction {
+        Renamed(String),
+        Cleared,
+    }
+
+    fn name_reducer(state: &mut Name, action: &NameAction) {
+        use NameAction::*;
+
+        match action {
+            Renamed(name) => state.0 = name.clone(),
+            Cleared => state.0.clear(),
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct Count(isize);
+
+    enum CountAction {
+        Incremented,
+        Decremented,
+    }
+
+    fn count_reducer(state: &mut Count, action: &CountAction) {
+        use CountAction::*;
+
+        match action {
+            Incremented => state.0 += 1,
+            Decremented => state.0 -= 1,
+        }
+    }
+
+    #[test]
+    fn test() {
+        let mut store = Store::new();
+
+        store.add_state(Count(0));
+        store.add_reducer(count_reducer);
+
+        store.add_state(Name("Alice".to_string()));
+        store.add_reducer(name_reducer);
+
+        store.dispatch(CountAction::Incremented);
+        store.dispatch(CountAction::Incremented);
+        store.dispatch(CountAction::Decremented);
+
+        store.dispatch(NameAction::Renamed("Bob".to_string()));
+
+        assert_eq!(*store.get_state::<Count>().unwrap(), Count(1));
+        assert_eq!(*store.get_state::<Name>().unwrap(), Name("Bob".to_string()));
     }
 }
