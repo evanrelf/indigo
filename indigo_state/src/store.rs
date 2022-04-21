111145065b14e139abbaff6b00d24dce3e0c6d20
Evan Relf <evan@evanrelf.com>
2022-04-20 19:22:57 -0700

    Rename example to "basic"

--- indigo_state/src/store.rs
+++ indigo_state/src/store.rs
@@ -10,7 +10,7 @@ use std::{
 /// # Examples
 ///
 /// ```
-#[doc = include_str!("../examples/example.rs")]
+#[doc = include_str!("../examples/basic.rs")]
 /// ```
 #[derive(Default)]
 pub struct Store<S = TypeMap> {
