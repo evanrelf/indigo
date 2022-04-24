27df85e43f062e21c6711e1288020d615dc97d9c
Evan Relf <evan@evanrelf.com>
2022-04-24 13:48:07 -0700

    Implement basic `Buffer` type, read and write file

---
+++ indigo_core/src/buffer.rs
@@ -0,0 +1,45 @@
+use ropey::Rope;
+use std::{
+    fs::File,
+    io::{self, BufReader, BufWriter},
+    path::{Path, PathBuf},
+};
+
+#[derive(Default)]
+pub struct Buffer {
+    pub path: Option<PathBuf>,
+    pub contents: Rope,
+}
+
+impl Buffer {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn open<P>(path: P) -> Result<Self, io::Error>
+    where
+        P: AsRef<Path>,
+    {
+        let path_buf = path.as_ref().to_path_buf();
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        Ok(Self {
+            path: Some(path_buf),
+            contents: Rope::from_reader(reader)?,
+        })
+    }
+
+    pub fn save(&self) -> Result<(), io::Error> {
+        if let Some(path) = &self.path {
+            let file = File::open(path)?;
+            let writer = BufWriter::new(file);
+            self.contents.write_to(writer)
+        } else {
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                "no path associated with buffer",
+            ))
+        }
+    }
+}
