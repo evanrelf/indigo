685f94bc757920ee75794e332b5b4b9af0539845
Evan Relf <evan@evanrelf.com>
2022-04-24 14:12:42 -0700

    Enable some additional Clippy lints

--- indigo_core/src/lib.rs
+++ indigo_core/src/lib.rs
@@ -1,3 +1,10 @@
+#![warn(
+    clippy::use_self,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss
+)]
+
 pub mod buffer;
 pub mod position;
 pub mod range;
