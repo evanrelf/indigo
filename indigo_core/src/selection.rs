f1737790af923807c1b8c415e4eb1e10604e12c6
Evan Relf <evan@evanrelf.com>
2022-04-24 16:06:24 -0700

    Track index for primary `Range` in `Selection`

--- indigo_core/src/selection.rs
+++ indigo_core/src/selection.rs
@@ -6,6 +6,7 @@ use crate::range::Range;
 // - Ranges must not overlap
 pub struct Selection {
     ranges: Vec<Range>,
+    primary_range: usize,
 }
 
 impl Selection {
@@ -13,12 +14,18 @@ impl Selection {
     pub fn ranges(&self) -> &Vec<Range> {
         &self.ranges
     }
+
+    #[must_use]
+    pub fn primary_range(&self) -> usize {
+        self.primary_range
+    }
 }
 
 impl Default for Selection {
     fn default() -> Self {
         Self {
             ranges: vec![Range::default()],
+            primary_range: 0,
         }
     }
 }
@@ -27,6 +34,7 @@ impl From<Range> for Selection {
     fn from(range: Range) -> Self {
         Self {
             ranges: vec![range],
+            primary_range: 0,
         }
     }
 }
