ba9b1a086ce853a9cb98a593d9cea90f23cad846
Evan Relf <evan@evanrelf.com>
2022-04-25 15:32:34 -0700

    Set up main loop

--- indigo_tui/src/lib.rs
+++ indigo_tui/src/lib.rs
@@ -5,4 +5,35 @@
     clippy::cast_sign_loss
 )]
 
+use crate::terminal::with_terminal;
+use indigo_core::editor::Editor;
+use tui::widgets::Widget;
+
 pub mod terminal;
+
+pub fn run(mut editor: Editor) {
+    with_terminal(|terminal| loop {
+        terminal
+            .draw(|frame| frame.render_widget(EditorWidget(&editor), frame.size()))
+            .unwrap();
+
+        let event = crossterm::event::read().unwrap();
+        handle_event(&mut editor, event);
+    });
+}
+
+pub struct EditorWidget<'editor>(pub &'editor Editor);
+
+impl Widget for EditorWidget<'_> {
+    fn render(self, area: tui::layout::Rect, buffer: &mut tui::buffer::Buffer) {
+        render_editor(self.0, area, buffer);
+    }
+}
+
+pub fn render_editor(editor: &Editor, area: tui::layout::Rect, buffer: &mut tui::buffer::Buffer) {
+    todo!();
+}
+
+pub fn handle_event(editor: &mut Editor, event: crossterm::event::Event) {
+    todo!();
+}
