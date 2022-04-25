f7184627bae3c21ed0837f68f0d0b5ea5f1f6d15
Evan Relf <evan@evanrelf.com>
2022-04-25 13:11:15 -0700

    Delete `indigo_state`
    Published at https://github.com/evanrelf/indux

--- indigo_state/src/field.rs
+++
@@ -1,7 +0,0 @@
-pub trait Field<T> {
-    type Error;
-
-    fn field(&self) -> Result<&T, Self::Error>;
-
-    fn field_mut(&mut self) -> Result<&mut T, Self::Error>;
-}
--- indigo_state/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![warn(clippy::use_self)]
-
-pub mod field;
-pub mod listener;
-pub mod reducer;
-pub mod store;
-pub mod type_map;
-
-pub use crate::{field::Field, store::Store, type_map::TypeMap};
