56ca4b339d5ca701166e214c4527da0abe8ba805
Evan Relf <evan@evanrelf.com>
2022-04-25 16:25:21 -0700

    Load paths from `indigo_tui` args

--- indigo_tui/src/main.rs
+++ indigo_tui/src/main.rs
@@ -1,3 +1,16 @@
+use indigo_core::Editor;
+
 fn main() {
-    indigo_tui::run(indigo_core::editor::Editor::default());
+    #[cfg(debug_assertions)]
+    if std::env::var("RUST_BACKTRACE").is_err() {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+
+    let mut editor = Editor::default();
+
+    for path in std::env::args().skip(1) {
+        editor.open_buffer(path).unwrap();
+    }
+
+    indigo_tui::run(editor);
 }
