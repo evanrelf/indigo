5a63a57b6c511cdc4a22af83e51ee2ee552b14cb
Evan Relf <evan@evanrelf.com>
2022-05-15 12:23:51 -0700

    Optionally write debug logging to file

--- indigo_tui/src/main.rs
+++ indigo_tui/src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser as _;
 use indigo_core::Editor;
-use std::path::PathBuf;
+use std::{fs::File, path::PathBuf};
 
 #[derive(clap::Parser)]
 struct Args {
+    #[clap(long)]
+    trace: Option<PathBuf>,
     files: Vec<PathBuf>,
 }
 
@@ -14,6 +16,18 @@ fn main() {
     }
 
     let args = Args::parse();
+
+    if let Some(trace_path) = args.trace {
+        let trace_file = if trace_path.exists() {
+            File::open(trace_path).unwrap()
+        } else {
+            File::create(trace_path).unwrap()
+        };
+        tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_writer(trace_file)
+            .init();
+    }
 
     let mut editor = Editor::default();
 
