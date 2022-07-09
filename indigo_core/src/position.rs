f427dd210c1acb436474e5a6673c8f6ec5268ef4
Evan Relf <evan@evanrelf.com>
2022-07-09 16:28:20 -0700

    Re-enable Unicode property tests

--- indigo_core/src/position.rs
+++ indigo_core/src/position.rs
@@ -175,18 +175,18 @@ mod test {
             let _ = Position::from_rope_index(rope, i);
         }
 
-        // #[test]
-        // fn test_to_rope_index_unicode(s in "\\PC+", line in 1usize.., column in 1usize..) {
-        //     let rope = &Rope::from(s);
-        //     let position = Position::try_from((line, column)).unwrap();
-        //     let _ = position.to_rope_index(rope).0.valid_for(rope).unwrap();
-        // }
-        //
-        // #[test]
-        // fn test_from_rope_index_unicode(s in "\\PC+", i: usize) {
-        //     let rope = &Rope::from(s);
-        //     let _ = Position::from_rope_index(rope, i).0.valid_for(rope).unwrap();
-        // }
+        #[test]
+        fn test_to_rope_index_unicode(s in "\\PC+", line in 1usize.., column in 1usize..) {
+            let rope = &Rope::from(s);
+            let position = Position::try_from((line, column)).unwrap();
+            let _ = position.to_rope_index(rope);
+        }
+
+        #[test]
+        fn test_from_rope_index_unicode(s in "\\PC+", i: usize) {
+            let rope = &Rope::from(s);
+            let _ = Position::from_rope_index(rope, i);
+        }
     }
 
     #[test]
