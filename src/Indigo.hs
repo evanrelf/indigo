b99f857cc0c1d3eec67430980037cf0f1550f795
Evan Relf <evan@evanrelf.com>
2023-06-17 12:19:27 -0700

    Start Haskell project

---
+++ src/Indigo.hs
@@ -0,0 +1,7 @@
+module Indigo
+  ( main
+  )
+where
+
+main :: IO ()
+main = putStrLn "Hello, world!"
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import qualified Indigo
+
+main :: IO ()
+main = Indigo.main
