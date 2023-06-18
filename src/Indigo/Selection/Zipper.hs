de14b5b86c2060b433d1f55dca3d14e0ec592c3a
Evan Relf <evan@evanrelf.com>
2023-06-17 22:39:20 -0700

    Add `isBegin`, `isEnd`, `begin`, `end`

--- src/Indigo/Selection/Zipper.hs
+++ src/Indigo/Selection/Zipper.hs
@@ -2,18 +2,34 @@
 
 module Indigo.Selection.Zipper
   ( SelectionZipper (..)
+
+    -- * Query
+  , isBegin
+  , isEnd
+
+    -- * Move
   , left
   , right
+  , begin
+  , end
   )
 where
 
 import Data.Sequence (Seq (..), (<|), (|>))
+
+import qualified Data.Sequence as Seq
 
 data SelectionZipper a = SelectionZipper
   { lefts :: Seq a
   , focus :: a
   , rights :: Seq a
   }
+
+isBegin :: SelectionZipper a -> Bool
+isBegin zipper = Seq.null zipper.lefts
+
+isEnd :: SelectionZipper a -> Bool
+isEnd zipper = Seq.null zipper.rights
 
 left :: SelectionZipper a -> SelectionZipper a
 left zipper =
@@ -53,4 +69,30 @@ right zipper =
         { lefts = Empty
         , focus
         , rights
+        }
+
+begin :: SelectionZipper a -> SelectionZipper a
+begin zipper =
+  case (zipper.lefts, zipper.rights) of
+    (Empty, _) ->
+      zipper
+
+    (focus :<| rights, _) ->
+      SelectionZipper
+        { lefts = Empty
+        , focus
+        , rights = rights <> one zipper.focus <> zipper.rights
+        }
+
+end :: SelectionZipper a -> SelectionZipper a
+end zipper =
+  case (zipper.lefts, zipper.rights) of
+    (_, Empty) ->
+      zipper
+
+    (_, lefts :|> focus) ->
+      SelectionZipper
+        { lefts = zipper.lefts <> one zipper.focus <> lefts
+        , focus
+        , rights = Empty
         }
