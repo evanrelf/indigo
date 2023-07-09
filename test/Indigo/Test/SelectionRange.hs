4b66b14a1be1705b85ddad9141c36840ee342a51
Evan Relf <evan@evanrelf.com>
2023-07-08 21:52:29 -0700

    Add tests for `SelectionRange`

---
+++ test/Indigo/Test/SelectionRange.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Indigo.Test.SelectionRange where
+
+import Hedgehog
+import Indigo.Core.SelectionRange
+import Indigo.Test.Position (genPosition)
+
+import qualified Hedgehog.Gen as Gen
+
+tests :: Group
+tests = $$(discover)
+
+prop_merge_is_commutative :: Property
+prop_merge_is_commutative = property do
+  r1 <- forAll genSelectionRange
+  r2 <- forAll genSelectionRange
+  r1 `merge` r2 === r2 `merge` r1
+
+prop_merge_is_associative :: Property
+prop_merge_is_associative = property do
+  r1 <- forAll genSelectionRange
+  r2 <- forAll genSelectionRange
+  r3 <- forAll genSelectionRange
+  (r1 `merge` r2) `merge` r3 === r1 `merge` (r2 `merge` r3)
+
+genSelectionRange :: Gen SelectionRange
+genSelectionRange = fromPositions <$> genPosition <*> genPosition
--- test/Main.hs
+++ test/Main.hs
@@ -4,6 +4,7 @@ import Prelude hiding (group)
 
 import qualified Indigo.Test.Position
 import qualified Indigo.Test.Range
+import qualified Indigo.Test.SelectionRange
 import qualified Test.Tasty as Tasty
 import qualified Test.Tasty.Hedgehog as Tasty.Hedgehog
 
@@ -12,4 +13,5 @@ main =
   Tasty.defaultMain $ Tasty.testGroup "Indigo" $ fmap Tasty.Hedgehog.fromGroup $
     [ Indigo.Test.Position.tests
     , Indigo.Test.Range.tests
+    , Indigo.Test.SelectionRange.tests
     ]
