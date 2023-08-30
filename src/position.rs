90503f42ef7ec9f69090ac6c97babae3577d0519
Evan Relf <evan@evanrelf.com>
2023-08-29 21:23:39 -0700

    Write tests, fix functions

--- src/position.rs
+++ src/position.rs
@@ -35,7 +35,7 @@ impl Position {
         }
     }
 
-    pub fn to_rope_index(self, rope: Rope) -> Conversion<usize> {
+    pub fn to_rope_index(&self, rope: Rope) -> Conversion<usize> {
         if rope.len_chars() == 0 {
             return Conversion::Invalid;
         }
@@ -43,16 +43,20 @@ impl Position {
         let mut corrected = false;
 
         let lines = rope.len_lines().saturating_sub(1);
-
         let line = if lines <= self.line {
-            lines.saturating_sub(1)
+            // When line goes beyond end of rope, use last line
+            corrected = true;
+            lines
         } else {
             self.line
         };
 
         let columns = rope.line(line).len_chars();
-
-        let column = if columns <= self.column {
+        let column = if corrected {
+            // When line was corrected, use last column
+            columns
+        } else if columns <= self.column {
+            // When column goes beyond end of line, use last column
             corrected = true;
             columns.saturating_sub(1)
         } else {
@@ -66,5 +70,64 @@ impl Position {
         } else {
             Conversion::Valid(index)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::conversion::Conversion;
+
+    #[test]
+    fn rope_length_empty() {
+        assert_eq!(Rope::default(), Rope::from_str(""));
+        assert_eq!(Rope::default().len_chars(), 0);
+        assert_eq!(Rope::default().len_lines(), 1); // empty rope has 1 line
+    }
+
+    #[test]
+    fn rope_length_single_line() {
+        assert_eq!(Rope::from_str("x").len_chars(), 1);
+        assert_eq!(Rope::from_str("x").len_lines(), 1); // no trailing newline == 1 line
+
+        assert_eq!(Rope::from_str("\n").len_chars(), 1);
+        assert_eq!(Rope::from_str("\n").len_lines(), 2);
+
+        assert_eq!(Rope::from_str("x\n").len_chars(), 2);
+        assert_eq!(Rope::from_str("x\n").len_lines(), 2); // trailing newline == 2 lines
+    }
+
+    #[test]
+    fn rope_length_multiple_lines() {
+        assert_eq!(Rope::from_str("x\ny\nz").len_chars(), 5);
+        assert_eq!(Rope::from_str("x\ny\nz").len_lines(), 3);
+
+        assert_eq!(Rope::from_str("x\ny\nz\n").len_chars(), 6);
+        assert_eq!(Rope::from_str("x\ny\nz\n").len_lines(), 4); // line count == '\n' count + 1
+    }
+
+    #[test]
+    fn to_rope_index_invalid() {
+        let rope = Rope::default();
+        let position = Position { line: 0, column: 0 };
+        assert_eq!(position.to_rope_index(rope), Conversion::Invalid);
+    }
+
+    #[test]
+    fn to_rope_index_corrected() {
+        let rope = Rope::from_str("foo\nbar");
+        let position = Position { line: 9, column: 0 };
+        assert_eq!(position.to_rope_index(rope), Conversion::Corrected(7));
+
+        let rope = Rope::from_str("foo\nbar\n");
+        let position = Position { line: 9, column: 0 };
+        assert_eq!(position.to_rope_index(rope), Conversion::Corrected(8));
+    }
+
+    #[test]
+    fn to_rope_index_valid() {
+        let rope = Rope::from_str("foo\nbar\n");
+        let position = Position { line: 1, column: 3 };
+        assert_eq!(position.to_rope_index(rope), Conversion::Valid(7));
     }
 }
