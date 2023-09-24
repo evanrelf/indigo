946c30d17aa6c8598d2d39215f2cb4ee70b9a890
Evan Relf <evan@evanrelf.com>
2023-09-24 14:40:01 -0700

    Start working on `Command` abstraction

--- src/Indigo/Core/Command.hs
+++ src/Indigo/Core/Command.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE ApplicativeDo #-}
 
 module Indigo.Core.Command
-  ( help
+  ( Command (..)
+  , run
+  , help
   )
 where
 
@@ -11,20 +13,47 @@ import Prelude hiding (void)
 
 -- TODO: Use `optparse-applicative`'s completion support
 
-help :: Maybe Options.ParserPrefs -> Options.Parser a -> Text -> [Text] -> Text
-help mPrefs parser0 name args0 = do
-  let prefs = fromMaybe Options.defaultPrefs mPrefs
+-- TODO: Should this have an `extract :: o -> a` field to make the parse result
+-- existential? If so, should it just return `a`, or something more constrained
+-- like `s -> m s`? Should we put a `Show` constraint (or whatever) on `o` for
+-- debugging? Not sure, just ideas.
+data Command a = Command
+  { name :: Text
+  , parser :: Options.Parser a
+  }
+  deriving stock (Functor)
+
+run :: Command a -> [Text] -> Either Text a
+run Command{name, parser} args = do
+  let info = Options.info parser mempty
+  let result = Options.execParserPure parserPrefs info (toString <$> args)
+  case result of
+    Options.CompletionInvoked _ -> error "unreachable"
+    Options.Success x -> Right x
+    Options.Failure failure -> do
+      let (helpText, _exitCode) = Options.renderFailure failure (toString name)
+      Left $ toText helpText
+
+help :: Command a -> [Text] -> Text
+help Command{name, parser} args = help' Nothing parser name args
+
+help' :: Maybe Options.ParserPrefs -> Options.Parser a -> Text -> [Text] -> Text
+help' mPrefs parser0 name args0 = do
+  let prefs = fromMaybe parserPrefs mPrefs
   let helpName = "INDIGO-INTERNAL-HELP"
   let parser = Options.helperWith (Options.long helpName) <*> (parser0 *> empty)
   let info = Options.info parser mempty
   let args = (toString <$> args0) <> ["--" <> helpName]
   let result = Options.execParserPure prefs info args
   case result of
-    Options.Success void -> absurd void
     Options.CompletionInvoked _ -> error "unreachable"
+    Options.Success void -> absurd void
     Options.Failure failure -> do
       let (helpText, _exitCode) = Options.renderFailure failure (toString name)
       toText helpText
+
+parserPrefs :: Options.ParserPrefs
+parserPrefs = Options.defaultPrefs
 
 _example :: Options.Parser ()
 _example = do
