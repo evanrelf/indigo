7158495af3a936249427d92da35b6768b3db7225
Evan Relf <evan@evanrelf.com>
2023-10-31 21:18:30 -0700

    Tweak `Selection`

--- src/selection.rs
+++ src/selection.rs
@@ -1,25 +1,40 @@
 use crate::{direction::Direction, range::Range, selection_range::SelectionRange};
+use std::collections::BTreeMap;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Selection {
-    ranges: Vec<(SelectionRange, Option<usize>)>,
+    primary: (SelectionRange, Option<usize>),
+    secondaries: BTreeMap<SelectionRange, Vec<Option<usize>>>,
     direction: Direction,
 }
 
 impl Selection {
-    pub fn ranges(&self) -> &Vec<(SelectionRange, Option<usize>)> {
-        &self.ranges
+    pub fn ranges(&self) -> ! {
+        todo!()
+    }
+
+    pub fn primary(&self) -> &(SelectionRange, Option<usize>) {
+        &self.primary
+    }
+
+    pub fn secondaries(&self) -> ! {
+        todo!()
     }
 
     pub fn direction(&self) -> &Direction {
         &self.direction
+    }
+
+    pub fn is_valid(&self) -> bool {
+        todo!()
     }
 }
 
 impl From<Range> for Selection {
     fn from(range: Range) -> Self {
         Self {
-            ranges: vec![(SelectionRange::from(range.clone()), range.target_column())],
+            primary: (SelectionRange::from(range.clone()), range.target_column()),
+            secondaries: BTreeMap::new(),
             direction: range.direction(),
         }
     }
