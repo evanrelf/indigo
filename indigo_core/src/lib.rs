decaa40d49db7674b91e97f529ade73e4acbd7f4
Evan Relf <evan@evanrelf.com>
2023-11-03 19:33:45 -0700

    Add warnings for sketchy casts

--- indigo_core/src/lib.rs
+++ indigo_core/src/lib.rs
@@ -1,4 +1,12 @@
-#![warn(clippy::must_use_candidate, clippy::use_self)]
+#![warn(
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::must_use_candidate,
+    clippy::use_self
+)]
 
 pub mod buffer;
 pub mod conversion;
