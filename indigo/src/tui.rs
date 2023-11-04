bc05260f5b96705e1171f7c899cd155633fee1fd
Evan Relf <evan@evanrelf.com>
2023-11-03 23:01:14 -0700

    Render multiple sections

--- indigo/src/tui.rs
+++ indigo/src/tui.rs
@@ -1,7 +1,7 @@
 use crate::macros::key_matches;
 use crossterm::event::Event;
 use indigo_core::Editor;
-use ratatui::{prelude::Frame, widgets::Paragraph};
+use ratatui::{prelude::*, widgets::Paragraph};
 
 #[derive(Default)]
 pub struct Tui {
@@ -36,8 +36,50 @@ impl Tui {
     }
 
     pub fn view(&self, frame: &mut Frame) {
-        let area = frame.size();
+        let areas = areas(frame.size());
+        frame.render_widget(Paragraph::new("~"), areas.number);
+        frame.render_widget(Paragraph::new("Hello, world!"), areas.buffer);
+        frame.render_widget(Paragraph::new("status"), areas.status);
+        frame.render_widget(Paragraph::new("command"), areas.command);
+    }
+}
 
-        frame.render_widget(Paragraph::new(format!("{:#?}", self.editor)), area);
+struct Areas {
+    number: Rect,
+    buffer: Rect,
+    status: Rect,
+    command: Rect,
+}
+
+fn areas(area: Rect) -> Areas {
+    let vertical = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            // number + buffer
+            Constraint::Min(0),
+            // status
+            Constraint::Length(1),
+            // command
+            Constraint::Length(1),
+        ])
+        .split(area);
+
+    let number_width = 3; // TODO: Calculate from `Editor`
+
+    let horizontal = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            // number
+            Constraint::Length(number_width),
+            // buffer
+            Constraint::Min(0),
+        ])
+        .split(vertical[0]);
+
+    Areas {
+        number: horizontal[0],
+        buffer: horizontal[1],
+        status: vertical[1],
+        command: vertical[2],
     }
 }
