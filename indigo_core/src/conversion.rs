706f50ceb47b53b97246d2b07fa3edfa09a60185
Evan Relf <evan@evanrelf.com>
2023-11-04 22:20:18 -0700

    Add methods of leaving `Conversion`

--- indigo_core/src/conversion.rs
+++ indigo_core/src/conversion.rs
@@ -4,3 +4,39 @@ pub enum Conversion<T> {
     Corrected(T),
     Valid(T),
 }
+
+impl<T> Conversion<T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            Self::Invalid => panic!("called `Conversion::unwrap()` on an `Invalid` value"),
+            Self::Corrected(x) | Self::Valid(x) => x,
+        }
+    }
+
+    pub fn unwrap_or(self, default: T) -> T {
+        match self {
+            Self::Invalid => default,
+            Self::Corrected(x) | Self::Valid(x) => x,
+        }
+    }
+
+    pub fn unwrap_or_else<F>(self, op: F) -> T
+    where
+        F: FnOnce() -> T,
+    {
+        match self {
+            Self::Invalid => op(),
+            Self::Corrected(x) | Self::Valid(x) => x,
+        }
+    }
+
+    pub fn unwrap_or_default(self) -> T
+    where
+        T: Default,
+    {
+        match self {
+            Self::Invalid => T::default(),
+            Self::Corrected(x) | Self::Valid(x) => x,
+        }
+    }
+}
