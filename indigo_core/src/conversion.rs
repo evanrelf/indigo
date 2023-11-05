443666abab9183a8f9cfda78cf4e065c74a1ef2d
Evan Relf <evan@evanrelf.com>
2023-11-05 10:56:43 -0800

    Add `map` and `map_invalid` for conversion

--- indigo_core/src/conversion.rs
+++ indigo_core/src/conversion.rs
@@ -8,6 +8,28 @@ pub enum Conversion<T, E> {
 }
 
 impl<T, E> Conversion<T, E> {
+    pub fn map<U, F>(self, op: F) -> Conversion<U, E>
+    where
+        F: FnOnce(T) -> U,
+    {
+        match self {
+            Self::Invalid(e) => Conversion::Invalid(e),
+            Self::Corrected(t) => Conversion::Corrected(op(t)),
+            Self::Valid(t) => Conversion::Valid(op(t)),
+        }
+    }
+
+    pub fn map_invalid<F, O>(self, op: O) -> Conversion<T, F>
+    where
+        O: FnOnce(E) -> F,
+    {
+        match self {
+            Self::Invalid(e) => Conversion::Invalid(op(e)),
+            Self::Corrected(t) => Conversion::Corrected(t),
+            Self::Valid(t) => Conversion::Valid(t),
+        }
+    }
+
     pub fn unwrap(self) -> T
     where
         E: Debug,
