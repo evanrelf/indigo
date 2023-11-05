47396fe672f9712c9fc544637df6e6ec0c126bf8
Evan Relf <evan@evanrelf.com>
2023-11-04 21:53:46 -0700

    Simplify status bar

--- indigo/src/ui/status.rs
+++ indigo/src/ui/status.rs
@@ -1,15 +1,10 @@
-use indigo_core::{Conversion, Editor, Mode, Position};
+use indigo_core::{Editor, Position};
 use ratatui::{
     prelude::{Buffer as Surface, *},
     widgets::{Paragraph, Widget as _},
 };
 
 pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
-    let mode = match editor.mode() {
-        Mode::Normal(_) => "normal",
-        Mode::Insert(_) => "insert",
-    };
-
     let Some(buffer) = editor.current_buffer() else {
         // TODO: What should be shown when there are no buffers, or there is no current buffer?
         return;
@@ -19,18 +14,11 @@ pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
 
     let modified = if buffer.is_modified() { " [+]" } else { "" };
 
-    let position = {
-        let position @ Position { line, column } = buffer.selection().primary().cursor();
-        let index = match position.to_char_index(buffer.contents()) {
-            Conversion::Invalid => panic!("Position {line}:{column} is not valid char index"),
-            Conversion::Corrected(index) | Conversion::Valid(index) => index,
-        };
-        format!("{line}:{column}#{index}")
-    };
+    let Position { line, column } = buffer.selection().primary().cursor();
 
     let count = buffer.selection().ranges().len();
 
-    Paragraph::new(format!("{mode} {path}{modified} {position} ({count})"))
+    Paragraph::new(format!("{path}{modified} {line}:{column}/{count}"))
         .alignment(Alignment::Right)
         .render(area, surface);
 }
