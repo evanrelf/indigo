eb1c6f752de7703859ac3a2398dd22c769ec3d64
Evan Relf <evan@evanrelf.com>
2023-11-04 18:11:46 -0700

    Naive copy of `rust1` buffer rendering

--- indigo/src/ui/buffer.rs
+++ indigo/src/ui/buffer.rs
@@ -1,6 +1,27 @@
 use indigo_core::Editor;
 use ratatui::prelude::{Buffer as Surface, *};
 
-pub fn render(_editor: &Editor, area: Rect, surface: &mut Surface) {
-    surface.set_string(area.x, area.y, "Hello, world!", Style::default());
+pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
+    // TODO: Show contents for current buffer, show nothing if no buffers
+    let Some(buffer) = &editor.buffers.get(0) else {
+        return;
+    };
+
+    for y in area.top()..area.bottom() {
+        let line_index = usize::from(y) + buffer.vertical_scroll();
+
+        if let Some(line) = buffer.contents().get_line(line_index) {
+            if let Some(line) = line.get_slice(buffer.horizontal_scroll()..) {
+                surface.set_stringn(
+                    area.x,
+                    y,
+                    line.to_string(),
+                    usize::from(area.width),
+                    Style::default(),
+                );
+            }
+        } else {
+            break;
+        }
+    }
 }
