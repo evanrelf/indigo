8616b6e3936dbf84e3199fabb63cc614ce14e78a
Evan Relf <evan@evanrelf.com>
2023-11-04 17:08:00 -0700

    Sync `clippy` warnings between lib and bin

--- indigo/src/main.rs
+++ indigo/src/main.rs
@@ -1,6 +1,7 @@
-#![warn(clippy::pedantic)]
+#![warn(clippy::pedantic, clippy::use_self)]
 #![allow(
     clippy::bool_to_int_with_if,
+    clippy::missing_errors_doc,
     clippy::missing_panics_doc,
     clippy::module_name_repetitions
 )]
