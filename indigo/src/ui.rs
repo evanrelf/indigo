4137888fac6c73617408041e17303977ca949786
Evan Relf <evan@evanrelf.com>
2023-11-04 17:06:07 -0700

    Ditch `Areas` and `areas`

--- indigo/src/ui.rs
+++ indigo/src/ui.rs
@@ -13,50 +13,33 @@ pub struct Indigo;
 
 impl Widget for Indigo {
     fn render(self, area: Rect, surface: &mut Surface) {
-        let areas = areas(area);
-        Numbers.render(areas.numbers, surface);
-        Buffer.render(areas.buffer, surface);
-        Status.render(areas.status, surface);
-        Command.render(areas.command, surface);
-    }
-}
+        let vertical = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                // numbers + buffer
+                Constraint::Min(0),
+                // status
+                Constraint::Length(1),
+                // command
+                Constraint::Length(1),
+            ])
+            .split(area);
 
-struct Areas {
-    numbers: Rect,
-    buffer: Rect,
-    status: Rect,
-    command: Rect,
-}
+        let numbers_width = 3; // TODO: Calculate from `Editor`
 
-fn areas(area: Rect) -> Areas {
-    let vertical = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            // numbers + buffer
-            Constraint::Min(0),
-            // status
-            Constraint::Length(1),
-            // command
-            Constraint::Length(1),
-        ])
-        .split(area);
+        let horizontal = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                // numbers
+                Constraint::Length(numbers_width),
+                // buffer
+                Constraint::Min(0),
+            ])
+            .split(vertical[0]);
 
-    let numbers_width = 3; // TODO: Calculate from `Editor`
-
-    let horizontal = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            // numbers
-            Constraint::Length(numbers_width),
-            // buffer
-            Constraint::Min(0),
-        ])
-        .split(vertical[0]);
-
-    Areas {
-        numbers: horizontal[0],
-        buffer: horizontal[1],
-        status: vertical[1],
-        command: vertical[2],
+        Numbers.render(horizontal[0], surface);
+        Buffer.render(horizontal[1], surface);
+        Status.render(vertical[1], surface);
+        Command.render(vertical[2], surface);
     }
 }
