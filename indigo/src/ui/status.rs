ecdc35abb67335c07e7bb6e1625d7894e768fe57
Evan Relf <evan@evanrelf.com>
2023-11-04 18:13:12 -0700

    Naive copy of `rust1` status rendering

--- indigo/src/ui/status.rs
+++ indigo/src/ui/status.rs
@@ -1,6 +1,25 @@
-use indigo_core::Editor;
+use indigo_core::{Editor, Mode};
 use ratatui::prelude::{Buffer as Surface, *};
 
-pub fn render(_editor: &Editor, area: Rect, surface: &mut Surface) {
-    surface.set_string(area.x, area.y, "status", Style::default());
+pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
+    let mode = match editor.mode {
+        Mode::Normal(_) => "normal",
+        Mode::Insert(_) => "insert",
+    };
+
+    // TODO: Show path for current buffer, show something else if no buffers
+    let Some(buffer) = &editor.buffers.get(0) else {
+        return;
+    };
+
+    let path = buffer.path().display();
+
+    let modified = if buffer.is_modified() { " [+]" } else { "" };
+
+    surface.set_string(
+        area.x,
+        area.y,
+        format!("{mode} {path}{modified}"),
+        Style::default(),
+    );
 }
