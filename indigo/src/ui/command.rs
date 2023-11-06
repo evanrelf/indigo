6822a2091d2679c5d77d72bbf6950bb5363094e7
Evan Relf <evan@evanrelf.com>
2023-11-05 19:05:09 -0800

    Show yellow cursor in command mode

--- indigo/src/ui/command.rs
+++ indigo/src/ui/command.rs
@@ -6,10 +6,20 @@ pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
         return;
     };
 
+    let yellow = Color::Rgb(0xFF, 0xD3, 0x3D);
+    // let light_yellow = Color::Rgb(0xFF, 0xF5, 0xB1);
+
     surface.set_string(
         area.x,
         area.y,
         format!(":{}", command_mode.command()),
         Style::default(),
     );
+
+    surface
+        .get_mut(
+            area.x + u16::try_from(command_mode.cursor()).unwrap() + 1,
+            area.y,
+        )
+        .set_bg(yellow);
 }
