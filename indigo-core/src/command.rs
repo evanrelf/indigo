8440a288cb048af60ca4d58326c2569267598be1
Evan Relf <evan@evanrelf.com>
2023-11-11 14:49:38 -0800

    Fix commands not working in command mode

--- indigo-core/src/command.rs
+++ indigo-core/src/command.rs
@@ -12,7 +12,9 @@ pub struct Quit {
 }
 
 pub fn parse(input: &str) -> anyhow::Result<Command> {
-    let words = shell_words::split(input)?;
+    // TODO: Remove requirement for command name (`indigo-core` in this case), while maintaining
+    // support for separate `indigo-core-command` bin.
+    let words = shell_words::split(&format!("indigo-core {input}"))?;
     let command = Command::try_parse_from(words)?;
     Ok(command)
 }
