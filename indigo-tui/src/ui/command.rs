19847fa870f6fcf5775ef98c47fe918509b78f0f
Evan Relf <evan@evanrelf.com>
2023-11-11 22:42:15 -0800

    Add todos for rendering command

--- indigo-tui/src/ui/command.rs
+++ indigo-tui/src/ui/command.rs
@@ -2,6 +2,10 @@ use crate::ui::colors::YELLOW;
 use indigo_core::{Editor, Mode};
 use ratatui::prelude::{Buffer as Surface, *};
 
+// TODO: Scroll (instead of panicking) when command length exceeds area width
+// TODO: Paint over status elements a few characters ahead of the command, so it's clear the command
+// text isn't part of the status
+
 pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
     let Mode::Command(command_mode) = editor.mode() else {
         return;
