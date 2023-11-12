681997a97c602f90b753ce54e55e3f366bcc7824
Evan Relf <evan@evanrelf.com>
2023-11-11 21:40:52 -0800

    Rename `areas` to `Areas::new`

--- indigo-tui/src/ui.rs
+++ indigo-tui/src/ui.rs
@@ -11,7 +11,7 @@ use ratatui::prelude::{Buffer as Surface, *};
 use std::cmp::max;
 
 pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
-    let areas = areas(editor, area);
+    let areas = Areas::new(editor, area);
     numbers::render(editor, areas.numbers, surface);
     buffer::render(editor, areas.buffer, surface);
     selection::render(editor, areas.buffer, surface);
@@ -27,39 +27,41 @@ pub struct Areas {
     command: Rect,
 }
 
-pub fn areas(editor: &Editor, area: Rect) -> Areas {
-    let vertical = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            // numbers + buffer
-            Constraint::Min(0),
-            // status
-            Constraint::Length(1),
-            // command
-            Constraint::Length(1),
-        ])
-        .split(area);
+impl Areas {
+    pub fn new(editor: &Editor, area: Rect) -> Self {
+        let vertical = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                // numbers + buffer
+                Constraint::Min(0),
+                // status
+                Constraint::Length(1),
+                // command
+                Constraint::Length(1),
+            ])
+            .split(area);
 
-    let numbers_width = {
-        let n = editor.current_buffer().contents().len_lines_indigo();
-        let digits = 1 + max(1, n).ilog10();
-        u16::try_from(max(2, digits) + 1).unwrap()
-    };
+        let numbers_width = {
+            let n = editor.current_buffer().contents().len_lines_indigo();
+            let digits = 1 + max(1, n).ilog10();
+            u16::try_from(max(2, digits) + 1).unwrap()
+        };
 
-    let horizontal = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            // numbers
-            Constraint::Length(numbers_width),
-            // buffer
-            Constraint::Min(0),
-        ])
-        .split(vertical[0]);
+        let horizontal = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                // numbers
+                Constraint::Length(numbers_width),
+                // buffer
+                Constraint::Min(0),
+            ])
+            .split(vertical[0]);
 
-    Areas {
-        numbers: horizontal[0],
-        buffer: horizontal[1],
-        status: vertical[1],
-        command: vertical[2],
+        Self {
+            numbers: horizontal[0],
+            buffer: horizontal[1],
+            status: vertical[1],
+            command: vertical[2],
+        }
     }
 }
