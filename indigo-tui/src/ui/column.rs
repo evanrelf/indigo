839b6f8b42f9afb2a5fea9d09ea7568eb3636424
Evan Relf <evan@evanrelf.com>
2023-11-11 22:26:19 -0800

    Support coloring multiple columns

--- indigo-tui/src/ui/column.rs
+++ indigo-tui/src/ui/column.rs
@@ -5,20 +5,30 @@ use ratatui::prelude::{Buffer as Surface, *};
 pub fn render(editor: &Editor, area: Rect, surface: &mut Surface) {
     let buffer = editor.current_buffer();
 
+    let horizontal_scroll = u16::try_from(buffer.horizontal_scroll()).unwrap();
+
     let total_lines = buffer.contents().len_lines_indigo();
 
-    let column = 81;
+    let columns = [81];
+
+    if columns.is_empty() {
+        return;
+    }
 
     for y in area.top()..area.bottom() {
         let line_number = usize::from(y) + buffer.vertical_scroll() + 1;
 
-        let column_in_area = area.x + (column - 1) < area.right();
-        let column_in_buffer = buffer.horizontal_scroll() < usize::from(column);
         let line_in_buffer = line_number <= total_lines;
 
-        if column_in_area && column_in_buffer && line_in_buffer {
-            let x = area.x + ((column - 1) - u16::try_from(buffer.horizontal_scroll()).unwrap());
-            surface.get_mut(x, y).set_bg(GRAY_LIGHT);
+        for column in columns {
+            let column_in_area = area.x + (column - 1) < area.right();
+
+            let column_in_buffer = horizontal_scroll < column;
+
+            if column_in_area && column_in_buffer && line_in_buffer {
+                let x = area.x + ((column - 1) - horizontal_scroll);
+                surface.get_mut(x, y).set_bg(GRAY_LIGHT);
+            }
         }
     }
 }
