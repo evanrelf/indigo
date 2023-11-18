d5f6ada7624104d74880cb0ff932cba86b9894a1
Evan Relf <evan@evanrelf.com>
2023-11-17 21:50:46 -0800

    Add stricter position conversion functions

--- indigo-core/src/position.rs
+++ indigo-core/src/position.rs
@@ -9,11 +9,21 @@ pub struct Position {
 
 impl Position {
     pub fn from_char_index(index: usize, rope: &Rope) -> anyhow::Result<Self> {
+        Self::from_char_index_strict(index, rope).map(|x| match x {
+            Ok(position) | Err(position) => position,
+        })
+    }
+
+    pub fn from_char_index_strict(index: usize, rope: &Rope) -> anyhow::Result<Result<Self, Self>> {
+        let mut corrected = false;
+
         if rope.len_chars() == 0 {
             anyhow::bail!("Rope is empty");
         }
 
         let index = if rope.len_chars() <= index {
+            corrected = true;
+            // When index goes beyond end of rope, use last index
             rope.len_chars().saturating_sub(1)
         } else {
             index
@@ -25,15 +35,26 @@ impl Position {
 
         let position = Self { line, column };
 
-        Ok(position)
+        if corrected {
+            Ok(Err(position))
+        } else {
+            Ok(Ok(position))
+        }
     }
 
     pub fn to_char_index(self, rope: &Rope) -> anyhow::Result<usize> {
+        self.to_char_index_strict(rope).map(|x| match x {
+            Ok(index) | Err(index) => index,
+        })
+    }
+
+    pub fn to_char_index_strict(self, rope: &Rope) -> anyhow::Result<Result<usize, usize>> {
         if rope.len_chars() == 0 {
             anyhow::bail!("Rope is empty");
         }
 
         let mut line_corrected = false;
+        let mut column_corrected = false;
 
         let last_line = rope.len_lines_indigo().saturating_sub(1);
         let line = if self.line > last_line {
@@ -47,6 +68,7 @@ impl Position {
         let last_column = rope.line(line).len_chars().saturating_sub(1);
         let column = if line_corrected || self.column > last_column {
             // When line was corrected, or column goes beyond end of line, use last column
+            column_corrected = true;
             last_column
         } else {
             self.column
@@ -54,7 +76,11 @@ impl Position {
 
         let index = column + rope.line_to_char(line);
 
-        Ok(index)
+        if line_corrected || column_corrected {
+            Ok(Err(index))
+        } else {
+            Ok(Ok(index))
+        }
     }
 
     pub fn correct(&mut self, rope: &Rope) {
