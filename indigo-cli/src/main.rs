6aef5d6e79d64d83311c9b5acc791160381571e4
Evan Relf <evan@evanrelf.com>
2023-12-10 16:08:40 -0800

    Use `tokio` in `indigo-cli`

--- indigo-cli/src/main.rs
+++ indigo-cli/src/main.rs
@@ -10,7 +10,8 @@ struct Args {
     keys: Keys,
 }
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     #[cfg(debug_assertions)]
     if std::env::var("RUST_BACKTRACE").is_err() {
         std::env::set_var("RUST_BACKTRACE", "1");
