d1fdb868e8c5cd3f6d51ba9c7d0aa5ec21355b77
Evan Relf <evan@evanrelf.com>
2024-02-02 21:25:42 -0800

    Add scrollbar arrows

--- indigo-tui/src/ui/scrollbar.rs
+++ indigo-tui/src/ui/scrollbar.rs
@@ -1,11 +1,29 @@
-use crate::ui::colors::GRAY_300;
+use crate::ui::colors::GRAY_400;
 use indigo_core::Editor;
 use ratatui::prelude::{Buffer as Surface, *};
 
 pub fn render(_editor: &Editor, area: Rect, surface: &mut Surface) {
-    for y in area.top()..(area.bottom() / 2) {
+    let color = GRAY_400;
+
+    for x in area.left()..area.right() {
+        // Up arrow
+        surface.get_mut(x, area.top()).set_char('▲').set_fg(color);
+
+        // Down arrow
+        surface
+            .get_mut(x, area.bottom() - 1)
+            .set_char('▼')
+            .set_fg(color);
+    }
+
+    for y in (area.top() + 1)..(area.bottom() - 1) {
+        // TODO: Remove
+        if y > (area.bottom() / 2) {
+            break;
+        }
+
         for x in area.left()..area.right() {
-            surface.get_mut(x, y).set_bg(GRAY_300);
+            surface.get_mut(x, y).set_bg(color);
         }
     }
 }
