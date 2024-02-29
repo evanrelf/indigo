f1e6bee2bb95fddea58a1804dbaa8280fa8e8d39
Evan Relf <evan@evanrelf.com>
2024-02-28 22:35:51 -0800

    Incomplete and bad copy of Solid's signals

--- crates/indigo-nursery/src/signal.rs
+++ crates/indigo-nursery/src/signal.rs
@@ -1,71 +1,54 @@
-use async_broadcast::{broadcast, Receiver, RecvError, Sender};
+use std::sync::{Arc, RwLock};
 
-pub fn signal<T>(value: T) -> (Writer<T>, Reader<T>) {
-    let (mut sender, receiver) = broadcast(1);
-    sender.set_overflow(true);
-    sender.set_await_active(false);
-    let writer = Writer { value, sender };
-    let reader = Reader { receiver };
-    (writer, reader)
+// https://www.youtube.com/watch?v=cELFZQAMdhQ
+
+pub fn signal<T>(value: T) -> (Getter<T>, Setter<T>) {
+    let value = Arc::new(RwLock::new(value));
+
+    let subscribers = Arc::new(RwLock::new(Vec::new()));
+
+    let getter = Getter {
+        value: Arc::clone(&value),
+        subscribers: Arc::clone(&subscribers),
+    };
+
+    let setter = Setter {
+        value: Arc::clone(&value),
+        subscribers: Arc::clone(&subscribers),
+    };
+
+    (getter, setter)
 }
 
-pub struct Writer<T> {
-    value: T,
-    sender: Sender<T>,
+pub struct Getter<T> {
+    value: Arc<RwLock<T>>,
+    #[allow(clippy::type_complexity)]
+    subscribers: Arc<RwLock<Vec<Box<dyn Fn(&T)>>>>,
 }
 
-impl<T> Writer<T> {
-    pub fn get(&self) -> &T {
-        &self.value
-    }
-
-    pub async fn set(&mut self, value: T)
+impl<T> Getter<T> {
+    pub fn get(&self) -> T
     where
         T: Clone,
     {
-        self.value = value;
-        self.notify().await;
-    }
-
-    pub async fn update<F>(&mut self, update: F)
-    where
-        T: Clone,
-        F: Fn(&mut T),
-    {
-        update(&mut self.value);
-        self.notify().await;
-    }
-
-    async fn notify(&mut self)
-    where
-        T: Clone,
-    {
-        self.sender
-            .broadcast_direct(self.value.clone())
-            .await
-            // TODO: Check if this is okay (will it error if nobody's listening?)
-            .unwrap();
+        (*self.value.read().unwrap()).clone()
     }
 }
 
-#[derive(Clone)]
-pub struct Reader<T> {
-    receiver: Receiver<T>,
+pub struct Setter<T> {
+    value: Arc<RwLock<T>>,
+    #[allow(clippy::type_complexity)]
+    subscribers: Arc<RwLock<Vec<Box<dyn Fn(&T)>>>>,
 }
 
-// TODO: "Get current value" vs "wait for next change"
-impl<T> Reader<T> {
-    pub async fn get(&mut self) -> Result<T, RecvError>
-    where
-        T: Clone,
-    {
-        // TODO: Could this loop forever, never going from "error overflowed" to "ok"?
-        loop {
-            match self.receiver.recv_direct().await {
-                Err(RecvError::Overflowed(_)) => continue,
-                Ok(value) => return Ok(value),
-                Err(RecvError::Closed) => return Err(RecvError::Closed),
-            }
+impl<T> Setter<T> {
+    pub fn set(&self, value: T) {
+        let mut value_guard = self.value.write().unwrap();
+
+        *value_guard = value;
+
+        for subscriber in &*self.subscribers.read().unwrap() {
+            subscriber(&value_guard);
         }
     }
 }
@@ -75,25 +58,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        smol::block_on(async {
-            let (mut writer, mut reader) = signal(0);
-
-            writer.update(|x| *x += 1).await;
-
-            let x = writer.get();
-            assert_eq!(*x, 1);
-
-            let x = reader.get().await.unwrap();
-            assert_eq!(x, 1);
-
-            writer.set(42).await;
-
-            let x = writer.get();
-            assert_eq!(*x, 42);
-
-            let x = reader.get().await.unwrap();
-            assert_eq!(x, 42);
-        });
+    fn single() {
+        let (count, set_count) = signal(0);
+        set_count.set(count.get() + 1);
+        assert_eq!(count.get(), 1);
     }
 }
