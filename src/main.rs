dcc2f4d493a5f1acd766d798858675bee087f09f
Evan Relf <evan@evanrelf.com>
2024-03-07 21:26:51 -0800

    Add command-line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use camino::Utf8PathBuf;
+use clap::Parser as _;
 use ropey::Rope;
 
+#[derive(clap::Parser, Debug)]
+struct Args {
+    file: Option<Utf8PathBuf>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{args:?}");
+}
+
+#[derive(Default)]
 struct Editor {
     text: Rope,
     cursor: (usize, usize),
     scroll: usize,
-}
-
-fn main() {
-    println!("Hello, world!");
 }
