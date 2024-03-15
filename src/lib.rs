aea005c7afb4f2ceca063f31e49f9191ceede56f
Evan Relf <evan@evanrelf.com>
2024-03-14 21:31:35 -0700

    Create `Text` type wrapping `Rope`

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,12 @@ mod mode;
 mod position;
 mod rope;
 mod selection;
+mod text;
 mod world;
 
 pub(crate) use crate::world::World;
 
 pub use crate::{
     buffer::Buffer, conversion::Conversion, editor::Editor, mode::Mode, position::Position,
-    rope::RopeExt, selection::Selection,
+    rope::RopeExt, selection::Selection, text::Text,
 };
