9794e4cf7880d8fbc75bd59ecb46289ff6e732b5
Evan Relf <evan@evanrelf.com>
2024-03-25 22:00:19 -0700

    Add todo about inserting text at EOF

--- src/mode.rs
+++ src/mode.rs
@@ -2,5 +2,7 @@
 pub enum Mode {
     Normal,
     Goto,
+    // TODO: How to insert text at EOF? Cursor inserts to the left, and must live on a valid rope
+    // position.
     Insert { after: bool },
 }
