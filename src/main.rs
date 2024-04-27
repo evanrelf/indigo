26add93030f740b5c480cd71ca0aa220ff169b08
Evan Relf <evan@evanrelf.com>
2024-04-26 23:32:02 -0700

    Add `camino`

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use camino::Utf8PathBuf;
 use clap::Parser as _;
+use ropey::Rope;
+use std::{fs::File, io::BufReader};
 
 #[derive(Debug, clap::Parser)]
-struct Args {}
+struct Args {
+    file: Utf8PathBuf,
+}
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    println!("{args:?}");
+    let file = File::open(args.file)?;
+
+    let rope = Rope::from_reader(BufReader::new(file))?;
+
+    println!("Lines: {}", rope.len_lines());
 
     Ok(())
 }
