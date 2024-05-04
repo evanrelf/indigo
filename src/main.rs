e34b96fc9f4ec0461d3b29ee8f30b7a2d2f7a0ea
Evan Relf <evan@evanrelf.com>
2024-05-03 20:07:14 -0700

    Start rendering something to the terminal

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod terminal;
 
 use camino::Utf8PathBuf;
 use clap::Parser as _;
+use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use ratatui::widgets::{Paragraph, Widget as _};
 use ropey::Rope;
 use std::{fs::File, io::BufReader};
 
@@ -30,9 +32,33 @@ impl Editor {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let editor = Editor::new(args.file)?;
+    let mut editor = Editor::new(args.file)?;
 
-    println!("Lines: {}", editor.text.len_lines());
+    let mut terminal = terminal::enter()?;
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            let surface = frame.buffer_mut();
+            Paragraph::new(format!("Cursor: {}\n{}", editor.cursor, editor.text))
+                .render(area, surface);
+        })?;
+
+        #[allow(clippy::single_match)]
+        match event::read()? {
+            Event::Key(key_event) => match (key_event.modifiers, key_event.code) {
+                (KeyModifiers::NONE, KeyCode::Char('h')) => {
+                    editor.cursor = editor.cursor.saturating_sub(1);
+                }
+                (KeyModifiers::NONE, KeyCode::Char('l')) => {
+                    editor.cursor += 1;
+                }
+                (KeyModifiers::CONTROL, KeyCode::Char('c')) => break,
+                _ => {}
+            },
+            _ => {}
+        }
+    }
 
     Ok(())
 }
