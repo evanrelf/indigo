1bf2ec795af4dff97135fe2bf63ebf17c1066ed8
Evan Relf <evan@evanrelf.com>
2024-05-04 21:32:21 -0700

    Add actions for horizontal scrolling

--- src/actions.rs
+++ src/actions.rs
@@ -10,12 +10,24 @@ pub fn move_right(editor: &mut Editor) {
 
 pub fn scroll_up(editor: &mut Editor) {
     let line = editor.vertical_scroll().saturating_sub(3);
-    let char_offset = 0;
+    let char_offset = editor.horizontal_scroll();
     editor.scroll_to(line, char_offset);
 }
 
 pub fn scroll_down(editor: &mut Editor) {
     let line = editor.vertical_scroll() + 3;
-    let char_offset = 0;
+    let char_offset = editor.horizontal_scroll();
+    editor.scroll_to(line, char_offset);
+}
+
+pub fn scroll_left(editor: &mut Editor) {
+    let line = editor.vertical_scroll();
+    let char_offset = editor.horizontal_scroll().saturating_sub(1);
+    editor.scroll_to(line, char_offset);
+}
+
+pub fn scroll_right(editor: &mut Editor) {
+    let line = editor.vertical_scroll();
+    let char_offset = editor.horizontal_scroll() + 1;
     editor.scroll_to(line, char_offset);
 }
