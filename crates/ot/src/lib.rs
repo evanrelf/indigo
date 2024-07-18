c32f8e219f7652e396b27f195d2a6e205b26834d
Evan Relf <evan@evanrelf.com>
2024-07-17 22:17:45 -0700

    More incremental work on OT

--- crates/ot/src/lib.rs
+++ crates/ot/src/lib.rs
@@ -1,3 +1,4 @@
+use ropey::Rope;
 use serde::{Deserialize, Serialize};
 use std::rc::Rc;
 
@@ -19,10 +20,25 @@ pub trait Operation: Sized {
 */
 
 // TODO: More efficient encoding?
-#[derive(Clone, Deserialize, Serialize)]
-pub struct Edits(Vec<Edit>);
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Edits {
+    edits: Vec<Edit>,
+    // TODO: Track input (rope this can apply to) and output (length of rope after edits) lengths?
+}
 
 impl Edits {
+    #[must_use]
+    pub fn new() -> Self {
+        Self { edits: Vec::new() }
+    }
+
+    #[must_use]
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            edits: Vec::with_capacity(capacity),
+        }
+    }
+
     pub fn retain(&mut self, length: usize) {
         self.push(Edit::retain(length));
     }
@@ -37,24 +53,41 @@ impl Edits {
 
     pub fn push(&mut self, edit: Edit) {
         // Check if there's an existing edit to merge with
-        if let Some(last) = self.0.last_mut() {
+        if let Some(last) = self.edits.last_mut() {
             // There's an existing edit; try to merge
             if let Some(edit) = last.merge(edit) {
                 // Merge failed; push
-                self.0.push(edit);
+                self.edits.push(edit);
             } else {
                 // Merge succeeded
             }
         } else {
             // This is the first edit; push
-            self.0.push(edit);
+            self.edits.push(edit);
         }
+    }
+
+    pub fn compose(&self, other: &Self) -> anyhow::Result<Self> {
+        todo!()
+    }
+
+    pub fn transform(&self, other: &Self) -> anyhow::Result<(Self, Self)> {
+        todo!()
+    }
+
+    pub fn apply(&self, rope: &Rope) -> anyhow::Result<Rope> {
+        todo!()
+    }
+
+    #[must_use]
+    pub fn invert(&self, rope: &Rope) -> Self {
+        todo!()
     }
 }
 
 // TODO: Char length?
 // TODO: More efficient encoding?
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Edit {
     Retain(usize),
     Delete(usize),
@@ -81,7 +114,7 @@ impl Edit {
         use Edit::{Delete, Insert, Retain};
 
         match (self, other) {
-            // Discard no-ops
+            // Discard empty edits
             (_, Retain(0) | Delete(0)) => None,
             (_, Insert(r)) if r.is_empty() => None,
             (Insert(l), Insert(r)) if l.is_empty() => {
@@ -102,10 +135,79 @@ impl Edit {
                 None
             }
 
-            // TODO: Merge more edits (e.g. truncate an insert?)
+            // TODO: Merge more edits (e.g. cancel out delete "x" and insert "x")
 
             // Merge failed, return ownership of `other`
             (_, other) => Some(other),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: Write tests for `Edits`
+
+    #[test]
+    fn edit_merge_empty() {
+        // Retain 0
+        let mut e1 = Edit::retain(42);
+        let e2 = Edit::retain(0);
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Retain(42));
+
+        let mut e1 = Edit::retain(0);
+        let e2 = Edit::retain(42);
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Retain(42));
+
+        // Delete 0
+        let mut e1 = Edit::delete(42);
+        let e2 = Edit::delete(0);
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Delete(42));
+
+        let mut e1 = Edit::delete(0);
+        let e2 = Edit::delete(42);
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Delete(42));
+
+        // Insert ""
+        let mut e1 = Edit::insert("hello");
+        let e2 = Edit::insert("");
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Insert(Rc::from("hello")));
+
+        let mut e1 = Edit::insert("");
+        let e2 = Edit::insert("hello");
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Insert(Rc::from("hello")));
+    }
+
+    #[test]
+    fn edit_merge_grow() {
+        // Retain
+        let mut e1 = Edit::retain(25);
+        let e2 = Edit::retain(50);
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Retain(75));
+
+        // Delete
+        let mut e1 = Edit::delete(25);
+        let e2 = Edit::delete(50);
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Delete(75));
+
+        // Insert
+        let mut e1 = Edit::insert("hello");
+        let e2 = Edit::insert(" world");
+        assert_eq!(e1.merge(e2), None);
+        assert_eq!(e1, Edit::Insert(Rc::from("hello world")));
+    }
+
+    #[test]
+    fn edit_merge_shrink() {
+        // TODO: Add merging logic that shrinks edits
     }
 }
