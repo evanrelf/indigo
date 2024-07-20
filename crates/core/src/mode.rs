28ec10813466c85e358783f8ab603c7d03772982
Evan Relf <evan@evanrelf.com>
2024-07-20 13:42:10 -0700

    Parse mode from string

--- crates/core/src/mode.rs
+++ crates/core/src/mode.rs
@@ -1,6 +1,7 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
+use winnow::{ascii::Caseless, combinator::alt, prelude::*};
 
-#[derive(Default)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
 pub enum Mode {
     #[default]
     Normal,
@@ -14,4 +15,20 @@ impl Display for Mode {
             Self::Insert => write!(f, "insert"),
         }
     }
+}
+
+impl FromStr for Mode {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        mode.parse(s).map_err(|e| e.to_string())
+    }
+}
+
+fn mode(input: &mut &str) -> PResult<Mode> {
+    alt((
+        alt((Caseless("normal"), Caseless("n"))).value(Mode::Normal),
+        alt((Caseless("insert"), Caseless("i"))).value(Mode::Insert),
+    ))
+    .parse_next(input)
 }
