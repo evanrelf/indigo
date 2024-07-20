96cbe582a4d9bf9607c31f341528a1f867c2cebd
Evan Relf <evan@evanrelf.com>
2024-07-20 12:58:35 -0700

    Copy `key` module from `rust2`

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -2,6 +2,7 @@ pub mod actions;
 mod cursor;
 mod display_width;
 mod editor;
+mod key;
 mod mode;
 mod range;
 mod rope;
@@ -10,6 +11,7 @@ pub use crate::{
     cursor::{Cursor, CursorExt, CursorMut},
     display_width::DisplayWidth,
     editor::Editor,
+    key::{Key, KeyCode, KeyModifier, Keys},
     mode::Mode,
     range::{Range, RangeExt, RangeMut},
     rope::RopeExt,
