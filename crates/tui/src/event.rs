31b391bb3ed0b8c1e52fa79c6c764fb452e7647b
Evan Relf <evan@evanrelf.com>
2025-01-18 22:10:19 -0800

    Write `event_i2c` function

--- crates/tui/src/event.rs
+++ crates/tui/src/event.rs
@@ -1,4 +1,4 @@
-use crate::key::key_c2i;
+use crate::key::{key_c2i, key_i2c};
 use anyhow::anyhow;
 
 mod c {
@@ -12,6 +12,14 @@ mod i {
 pub fn event_c2i(event: &c::Event) -> anyhow::Result<i::Event> {
     match event {
         c::Event::Key(key_event) => Ok(i::Event::Key(key_c2i(*key_event)?)),
-        _ => Err(anyhow!("Unsupported crossterm event: {event:?}")),
+        _ => Err(anyhow!(
+            "Unsupported crossterm->indigo event conversion: {event:?}"
+        )),
+    }
+}
+
+pub fn event_i2c(event: &i::Event) -> c::Event {
+    match event {
+        i::Event::Key(key_event) => c::Event::Key(key_i2c(*key_event)),
     }
 }
