a9afa461dfc1bcbf8a4ae7a9d45fe274f8ca2038
Evan Relf <evan@evanrelf.com>
2025-02-22 11:42:40 -0800

    Start moving event handling into core

--- crates/core/src/event.rs
+++ crates/core/src/event.rs
@@ -1,4 +1,4 @@
-use crate::key::Key;
+use crate::{editor::Editor, key::Key, mode::Mode};
 
 pub enum Event {
     Key(Key),
@@ -16,4 +16,35 @@ impl Event {
 #[derive(Eq, Hash, PartialEq)]
 pub enum EventKind {
     Key,
+}
+
+pub fn handle_event(editor: &mut Editor, event: &Event) {
+    match editor.mode {
+        Mode::Normal(_) => handle_event_normal(editor, event),
+        Mode::Insert => handle_event_insert(editor, event),
+    }
+}
+
+pub fn handle_event_normal(editor: &mut Editor, event: &Event) {
+    let Mode::Normal(ref mut normal_mode) = editor.mode else {
+        unreachable!()
+    };
+
+    match event {
+        Event::Key(key) => match key {
+            _ => {}
+        },
+    }
+}
+
+pub fn handle_event_insert(editor: &mut Editor, event: &Event) {
+    let Mode::Insert = editor.mode else {
+        unreachable!()
+    };
+
+    match event {
+        Event::Key(key) => match key {
+            _ => {}
+        },
+    }
 }
