ea25ce3d7f5f5305d4f0c4215e636b3ea3073ef4
Evan Relf <evan@evanrelf.com>
2025-02-22 12:07:11 -0800

    Copy key event handling from `tui` to `core`

--- crates/core/src/event.rs
+++ crates/core/src/event.rs
@@ -1,4 +1,10 @@
-use crate::{editor::Editor, key::Key, mode::Mode};
+use crate::{
+    actions,
+    editor::Editor,
+    key::{Key, KeyCode, KeyModifier},
+    mode::Mode,
+};
+use std::num::NonZeroUsize;
 
 pub enum Event {
     Key(Key),
@@ -26,24 +32,66 @@ pub fn handle_event(editor: &mut Editor, event: &Event) {
 }
 
 pub fn handle_event_normal(editor: &mut Editor, event: &Event) {
+    use KeyCode as C;
+    use KeyModifier as M;
+
     let Mode::Normal(ref mut normal_mode) = editor.mode else {
         unreachable!()
     };
 
     match event {
-        Event::Key(key) => match key {
+        Event::Key(key) => match (key.modifiers, key.code) {
+            (m, C::Char(c @ ('0'..='9'))) if m.is_empty() => {
+                let n = usize::from(u8::try_from(c).unwrap() - b'0');
+                normal_mode.count = normal_mode
+                    .count
+                    .saturating_mul(NonZeroUsize::new(10).unwrap())
+                    .saturating_add(n);
+            }
+            (m, C::Escape) if m.is_empty() => actions::enter_normal_mode(editor),
+            (m, C::Char('i')) if m.is_empty() => actions::enter_insert_mode(editor),
+            // TODO: Add `a` for entering insert mode with the cursor moved to the right.
+            (m, C::Char('h')) if m.is_empty() => actions::move_left(editor),
+            (m, C::Char('l')) if m.is_empty() => actions::move_right(editor),
+            (m, C::Char('h' | 'H')) if m == M::Shift => actions::extend_left(editor),
+            (m, C::Char('l' | 'L')) if m == M::Shift => actions::extend_right(editor),
+            (m, C::Char(';')) if m.is_empty() => actions::reduce(editor),
+            (m, C::Char(';')) if m == M::Alt => actions::flip(editor),
+            (m, C::Char(';')) if m == M::Alt | M::Shift => actions::flip_forward(editor),
+            (m, C::Char('d')) if m.is_empty() => actions::delete(editor),
+            (m, C::Char('u')) if m == M::Control => actions::scroll_half_page_up(editor),
+            (m, C::Char('d')) if m == M::Control => actions::scroll_half_page_down(editor),
+            (m, C::Char('b')) if m == M::Control => actions::scroll_full_page_up(editor),
+            (m, C::Char('f')) if m == M::Control => actions::scroll_full_page_down(editor),
+            // (m, C::Char('l')) if m == M::Control => terminal.clear()?,
+            (m, C::Char('c')) if m == M::Control => editor.quit = true,
             _ => {}
         },
     }
 }
 
 pub fn handle_event_insert(editor: &mut Editor, event: &Event) {
+    use KeyCode as C;
+    use KeyModifier as M;
+
     let Mode::Insert = editor.mode else {
         unreachable!()
     };
 
     match event {
-        Event::Key(key) => match key {
+        Event::Key(key) => match (key.modifiers, key.code) {
+            (m, C::Escape) if m.is_empty() => actions::enter_normal_mode(editor),
+            (m, C::Backspace) if m.is_empty() => actions::delete_before(editor),
+            (m, C::Delete) if m.is_empty() => actions::delete_after(editor),
+            (m, C::Char(c)) if m.is_empty() => actions::insert_char(editor, c),
+            (m, C::Enter) if m.is_empty() => actions::insert_char(editor, '\n'),
+            (m, C::Tab) if m.is_empty() => actions::insert_char(editor, '\t'),
+            (m, C::Char('u')) if m == M::Control => actions::scroll_half_page_up(editor),
+            (m, C::Char('d')) if m == M::Control => actions::scroll_half_page_down(editor),
+            (m, C::Char('b')) if m == M::Control => actions::scroll_full_page_up(editor),
+            (m, C::Char('f')) if m == M::Control => actions::scroll_full_page_down(editor),
+            // (m, C::Char('l')) if m == M::Control => terminal.clear()?,
+            (m, C::Char('c')) if m == M::Control => editor.quit = true,
             _ => {}
         },
     }
