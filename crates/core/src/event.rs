cfe838f38f64f3ba175de30dba8d133ff678e0c5
Evan Relf <evan@evanrelf.com>
2025-02-22 14:04:22 -0800

    Add syntax sugar for matching on key events

--- crates/core/src/event.rs
+++ crates/core/src/event.rs
@@ -1,7 +1,7 @@
 use crate::{
     actions,
     editor::Editor,
-    key::{Key, KeyCode, KeyModifier},
+    key::{Key, KeyCode},
     mode::Mode,
 };
 use std::num::NonZeroUsize;
@@ -24,6 +24,16 @@ pub enum EventKind {
     Key,
 }
 
+#[expect(clippy::trivially_copy_pass_by_ref)]
+#[must_use]
+fn is(x: &Key, y: &str) -> bool {
+    let mut x = *x;
+    let mut y = y.parse::<Key>().unwrap();
+    x.normalize();
+    y.normalize();
+    x == y
+}
+
 pub fn handle_event(editor: &mut Editor, event: &Event) {
     match editor.mode {
         Mode::Normal(_) => handle_event_normal(editor, event),
@@ -32,66 +42,62 @@ pub fn handle_event(editor: &mut Editor, event: &Event) {
 }
 
 pub fn handle_event_normal(editor: &mut Editor, event: &Event) {
-    use KeyCode as C;
-    use KeyModifier as M;
-
     let Mode::Normal(ref mut normal_mode) = editor.mode else {
         unreachable!()
     };
 
+    let mut update_count = |c: char| {
+        let n = usize::from(u8::try_from(c).unwrap() - b'0');
+        normal_mode.count = normal_mode
+            .count
+            .saturating_mul(NonZeroUsize::new(10).unwrap())
+            .saturating_add(n);
+    };
+
     match event {
         Event::Key(key) => match (key.modifiers, key.code) {
-            (m, C::Char(c @ ('0'..='9'))) if m.is_empty() => {
-                let n = usize::from(u8::try_from(c).unwrap() - b'0');
-                normal_mode.count = normal_mode
-                    .count
-                    .saturating_mul(NonZeroUsize::new(10).unwrap())
-                    .saturating_add(n);
-            }
-            (m, C::Escape) if m.is_empty() => actions::enter_normal_mode(editor),
-            (m, C::Char('i')) if m.is_empty() => actions::enter_insert_mode(editor),
+            (m, KeyCode::Char(c @ '0'..='9')) if m.is_empty() => update_count(c),
+            _ if is(key, "<esc>") => actions::enter_normal_mode(editor),
+            _ if is(key, "i") => actions::enter_insert_mode(editor),
             // TODO: Add `a` for entering insert mode with the cursor moved to the right.
-            (m, C::Char('h')) if m.is_empty() => actions::move_left(editor),
-            (m, C::Char('l')) if m.is_empty() => actions::move_right(editor),
-            (m, C::Char('h' | 'H')) if m == M::Shift => actions::extend_left(editor),
-            (m, C::Char('l' | 'L')) if m == M::Shift => actions::extend_right(editor),
-            (m, C::Char(';')) if m.is_empty() => actions::reduce(editor),
-            (m, C::Char(';')) if m == M::Alt => actions::flip(editor),
-            (m, C::Char(';')) if m == M::Alt | M::Shift => actions::flip_forward(editor),
-            (m, C::Char('d')) if m.is_empty() => actions::delete(editor),
-            (m, C::Char('u')) if m == M::Control => actions::scroll_half_page_up(editor),
-            (m, C::Char('d')) if m == M::Control => actions::scroll_half_page_down(editor),
-            (m, C::Char('b')) if m == M::Control => actions::scroll_full_page_up(editor),
-            (m, C::Char('f')) if m == M::Control => actions::scroll_full_page_down(editor),
-            // (m, C::Char('l')) if m == M::Control => terminal.clear()?,
-            (m, C::Char('c')) if m == M::Control => editor.quit = true,
+            _ if is(key, "h") => actions::move_left(editor),
+            _ if is(key, "l") => actions::move_right(editor),
+            _ if is(key, "H") => actions::extend_left(editor),
+            _ if is(key, "L") => actions::extend_right(editor),
+            _ if is(key, ";") => actions::reduce(editor),
+            _ if is(key, "<a-;>") => actions::flip(editor),
+            _ if is(key, "<a-s-;>") => actions::flip_forward(editor),
+            _ if is(key, "d") => actions::delete(editor),
+            _ if is(key, "<c-u>") => actions::scroll_half_page_up(editor),
+            _ if is(key, "<c-d>") => actions::scroll_half_page_down(editor),
+            _ if is(key, "<c-b>") => actions::scroll_full_page_up(editor),
+            _ if is(key, "<c-f>") => actions::scroll_full_page_down(editor),
+            // _ if is(key, "<c-l>") => terminal.clear()?,
+            _ if is(key, "<c-c>") => editor.quit = true,
             _ => {}
         },
     }
 }
 
 pub fn handle_event_insert(editor: &mut Editor, event: &Event) {
-    use KeyCode as C;
-    use KeyModifier as M;
-
     let Mode::Insert = editor.mode else {
         unreachable!()
     };
 
     match event {
         Event::Key(key) => match (key.modifiers, key.code) {
-            (m, C::Escape) if m.is_empty() => actions::enter_normal_mode(editor),
-            (m, C::Backspace) if m.is_empty() => actions::delete_before(editor),
-            (m, C::Delete) if m.is_empty() => actions::delete_after(editor),
-            (m, C::Char(c)) if m.is_empty() => actions::insert_char(editor, c),
-            (m, C::Return) if m.is_empty() => actions::insert_char(editor, '\n'),
-            (m, C::Tab) if m.is_empty() => actions::insert_char(editor, '\t'),
-            (m, C::Char('u')) if m == M::Control => actions::scroll_half_page_up(editor),
-            (m, C::Char('d')) if m == M::Control => actions::scroll_half_page_down(editor),
-            (m, C::Char('b')) if m == M::Control => actions::scroll_full_page_up(editor),
-            (m, C::Char('f')) if m == M::Control => actions::scroll_full_page_down(editor),
-            // (m, C::Char('l')) if m == M::Control => terminal.clear()?,
-            (m, C::Char('c')) if m == M::Control => editor.quit = true,
+            _ if is(key, "<esc>") => actions::enter_normal_mode(editor),
+            _ if is(key, "<bs>") => actions::delete_before(editor),
+            _ if is(key, "<del>") => actions::delete_after(editor),
+            (m, KeyCode::Char(c)) if m.is_empty() => actions::insert_char(editor, c),
+            _ if is(key, "<ret>") => actions::insert_char(editor, '\n'),
+            _ if is(key, "<tab>") => actions::insert_char(editor, '\t'),
+            _ if is(key, "<c-u>") => actions::scroll_half_page_up(editor),
+            _ if is(key, "<c-d>") => actions::scroll_half_page_down(editor),
+            _ if is(key, "<c-b>") => actions::scroll_full_page_up(editor),
+            _ if is(key, "<c-f>") => actions::scroll_full_page_down(editor),
+            // _ if is(key, "<c-l>") => terminal.clear()?,
+            _ if is(key, "<c-c>") => editor.quit = true,
             _ => {}
         },
     }
