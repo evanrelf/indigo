f4108f6eb9ab5e3b78c23bafeb7ece5b5f0ffc08
Evan Relf <evan@evanrelf.com>
2025-03-08 22:26:41 -0800

    sim: Adjust formatting

--- crates/sim/src/main.rs
+++ crates/sim/src/main.rs
@@ -13,21 +13,26 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
     if let Some(seed) = args.seed {
         sim(seed);
-    } else {
-        let n = thread::available_parallelism().unwrap().get();
-        let mut threads = Vec::with_capacity(n);
-        for _ in 0..n {
-            let seed = fastrand::u64(..);
-            let handle = thread::spawn(move || sim(seed));
-            threads.push((seed, handle));
-        }
-        for (seed, handle) in threads {
-            if let Err(panic) = handle.join() {
-                println!("Indigo panicked! (seed: {seed})");
-                panic::resume_unwind(panic);
-            }
+        return;
+    }
+
+    let n = thread::available_parallelism().unwrap().get();
+
+    let mut threads = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        let seed = fastrand::u64(..);
+        let handle = thread::spawn(move || sim(seed));
+        threads.push((seed, handle));
+    }
+
+    for (seed, handle) in threads {
+        if let Err(panic) = handle.join() {
+            println!("Discovered a panic! (seed: {seed})");
+            panic::resume_unwind(panic);
         }
     }
 }
@@ -36,8 +41,10 @@ fn sim(seed: u64) {
     let mut raw_data = [0u8; 1024];
     fastrand::Rng::with_seed(seed).fill(&mut raw_data);
     let mut unstructured = arbitrary::Unstructured::new(&raw_data);
+
     let rope = Rope::from_str(unstructured.arbitrary::<&str>().unwrap());
     let mut editor = Editor::from_rope(rope);
+
     loop {
         let action = unstructured.arbitrary::<Action>().unwrap();
         handle_action(&mut editor, action);
