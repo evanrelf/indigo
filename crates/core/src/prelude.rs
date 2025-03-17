9b62a61dbc61358c583b04ee9fd58e9d1ea10c01
Evan Relf <evan@evanrelf.com>
2025-03-16 23:04:27 -0700

    Remove old non-view range and cursor code

--- crates/core/src/prelude.rs
+++ crates/core/src/prelude.rs
@@ -2,14 +2,12 @@
 pub use crate::{
     buffer::Buffer,
     cursor_view::{Cursor, CursorMut, CursorState},
-    // cursor::Cursor,
     display_width::DisplayWidth,
     editor::Editor,
     key::{Key, KeyCode, KeyModifier, Keys},
     mode::{CommandMode, InsertMode, Mode, NormalMode},
     ot::EditSeq,
     range_view::{Range, RangeMut, RangeState},
-    // range::{Range, RangeMut, RawRange},
     rope::{RopeExt, SnapBias},
 };
 
