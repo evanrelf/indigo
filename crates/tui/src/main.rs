230bf7d6a06a0f06c54c6e931c9257cd30eabb54
Evan Relf <evan@evanrelf.com>
2025-03-17 21:44:25 -0700

    Add option to print frame time stats

--- crates/tui/src/main.rs
+++ crates/tui/src/main.rs
@@ -1,6 +1,7 @@
 use camino::Utf8PathBuf;
 use clap::Parser as _;
 use etcetera::app_strategy::{AppStrategy as _, Xdg};
+use hdrhistogram::Histogram;
 use indigo_core::prelude::*;
 use indigo_tui::{
     areas::Areas,
@@ -10,7 +11,8 @@ use indigo_tui::{
     widgets,
 };
 use ratatui::{crossterm, widgets::Widget};
-use std::{fs, io, process::ExitCode, sync::Arc};
+use std::time::Instant;
+use std::{cmp::max, fs, io, process::ExitCode, sync::Arc};
 
 #[derive(Debug, clap::Parser)]
 struct Args {
@@ -18,6 +20,9 @@ struct Args {
 
     #[clap(long, env = "INDIGO_LOG", default_value_t)]
     log_filter: Arc<str>,
+
+    #[clap(long)]
+    stats: bool,
 }
 
 fn main() -> anyhow::Result<ExitCode> {
@@ -95,15 +100,19 @@ fn init_tracing_subscriber(args: &Args, xdg: &Xdg) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run(_args: &Args, terminal: &mut Terminal, mut editor: Editor) -> anyhow::Result<ExitCode> {
+fn run(args: &Args, terminal: &mut Terminal, mut editor: Editor) -> anyhow::Result<ExitCode> {
     let mut areas = Areas::default();
+
+    let mut frame_times = Histogram::<u16>::new(3)?;
+
+    let mut frame = Instant::now();
 
     // Using a non-continuous frame to omit time spent blocking on reading the next event. It makes
     // up the vast majority of time in the loop.
     #[cfg(feature = "tracy")]
     let mut tracy_frame = Some(tracing_tracy::client::non_continuous_frame!("frame"));
 
-    loop {
+    let exit_code = loop {
         terminal.draw(|frame| {
             let _span = tracing::trace_span!("terminal draw").entered();
             let area = frame.area();
@@ -113,10 +122,19 @@ fn run(_args: &Args, terminal: &mut Terminal, mut editor: Editor) -> anyhow::Res
             widgets::Editor::new(&editor).render(area, surface);
         })?;
 
+        if args.stats {
+            if let Ok(frame_time) = u64::try_from(frame.elapsed().as_micros()) {
+                let _ = frame_times.record(frame_time);
+            }
+        }
+
         #[cfg(feature = "tracy")]
         let _ = tracy_frame.take();
 
         let event = loop {
+            if args.stats {
+                frame = Instant::now();
+            }
             let event = crossterm::event::read()?;
             if !should_skip_event(&event) {
                 break event;
@@ -129,7 +147,31 @@ fn run(_args: &Args, terminal: &mut Terminal, mut editor: Editor) -> anyhow::Res
         handle_event(&mut editor, terminal, areas, event)?;
 
         if let Some(exit_code) = editor.exit {
-            return Ok(ExitCode::from(exit_code));
+            break ExitCode::from(exit_code);
         }
+    };
+
+    let _ = terminal::exit();
+
+    if args.stats {
+        println!("{} frames", frame_times.len());
+
+        let p50_micros = frame_times.value_at_quantile(0.50);
+        let p50_fps = 1_000_000 / max(1, p50_micros);
+        println!("P50:   {p50_micros:>6}μs {p50_fps:>4}fps");
+
+        let p90_micros = frame_times.value_at_quantile(0.90);
+        let p90_fps = 1_000_000 / max(1, p90_micros);
+        println!("P90:   {p90_micros:>6}μs {p90_fps:>4}fps");
+
+        let p999_micros = frame_times.value_at_quantile(0.999);
+        let p999_fps = 1_000_000 / max(1, p999_micros);
+        println!("P99.9: {p999_micros:>6}μs {p999_fps:>4}fps");
+
+        let max_micros = frame_times.max();
+        let max_fps = 1_000_000 / max(1, max_micros);
+        println!("Worst: {max_micros:>6}μs {max_fps:>4}fps");
     }
+
+    Ok(exit_code)
 }
