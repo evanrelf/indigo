63fd497bff9b4d8dd601d8a746278abd3d522ee5
Evan Relf <evan@evanrelf.com>
2025-04-26 14:53:23 -0700

    Make `read_input_system` public
    In case another system wants to explicitly schedule itself before or
    after.

--- crates/indigo-bevy/src/terminal.rs
+++ crates/indigo-bevy/src/terminal.rs
@@ -4,6 +4,21 @@ use std::time::Duration;
 #[derive(Deref, DerefMut, Resource)]
 pub struct Terminal(pub ratatui::DefaultTerminal);
 
+impl Terminal {
+    pub fn read_input_system(mut terminal_event: EventWriter<TerminalEvent>) -> Result {
+        use crossterm::event;
+
+        // TODO: `crossterm` says `poll` and `read` have to always run on the same thread, but neither
+        // Bevy nor I am guaranteeing this here!
+        while event::poll(Duration::ZERO)? {
+            let event = event::read()?;
+            terminal_event.write(TerminalEvent(event));
+        }
+
+        Ok(())
+    }
+}
+
 impl Drop for Terminal {
     fn drop(&mut self) {
         ratatui::restore();
@@ -20,19 +35,6 @@ impl Plugin for TerminalPlugin {
         let terminal = ratatui::init();
         app.insert_resource(Terminal(terminal))
             .add_event::<TerminalEvent>()
-            .add_systems(PreUpdate, read_input_system);
+            .add_systems(PreUpdate, Terminal::read_input_system);
     }
-}
-
-fn read_input_system(mut terminal_event: EventWriter<TerminalEvent>) -> Result {
-    use crossterm::event;
-
-    // TODO: `crossterm` says `poll` and `read` have to always run on the same thread, but neither
-    // Bevy nor I am guaranteeing this here!
-    while event::poll(Duration::ZERO)? {
-        let event = event::read()?;
-        terminal_event.write(TerminalEvent(event));
-    }
-
-    Ok(())
 }
