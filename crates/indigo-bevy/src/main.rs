a0d26dfbfcf3b9bde39b576f2e3229a4d1c461ac
Evan Relf <evan@evanrelf.com>
2025-04-26 16:24:21 -0700

    Only render when necessary

--- crates/indigo-bevy/src/main.rs
+++ crates/indigo-bevy/src/main.rs
@@ -2,7 +2,7 @@
 
 mod terminal;
 
-use crate::terminal::{Terminal, TerminalKeyEvent, TuiPlugin};
+use crate::terminal::{Terminal, TerminalKeyEvent, TerminalResizeEvent, TuiPlugin};
 use bevy::prelude::*;
 use clap::Parser as _;
 
@@ -11,6 +11,9 @@ struct Args {}
 
 #[derive(Deref, DerefMut, Resource)]
 struct Text(String);
+
+#[derive(Default, Event)]
+struct RenderEvent;
 
 fn main() {
     let args = Args::parse();
@@ -21,10 +24,12 @@ fn main() {
         .add_plugins((MinimalPlugins, TuiPlugin))
         .insert_resource(args)
         .insert_resource(text)
+        .add_event::<RenderEvent>()
+        .add_systems(Startup, render_system)
         .add_systems(
             Update,
             (
-                render_system,
+                render_system.run_if(on_event::<RenderEvent>.or(on_event::<TerminalResizeEvent>)),
                 handle_key_input_system.run_if(on_event::<TerminalKeyEvent>),
             ),
         )
@@ -45,9 +50,12 @@ fn render_system(text: Res<Text>, mut terminal: ResMut<Terminal>) -> Result {
 fn handle_key_input_system(
     mut text: ResMut<Text>,
     mut key_events: EventReader<TerminalKeyEvent>,
+    mut render_events: EventWriter<RenderEvent>,
     mut exit_events: EventWriter<AppExit>,
 ) {
     use crossterm::event::{KeyCode, KeyModifiers};
+
+    let mut should_render = false;
 
     for TerminalKeyEvent(key_event) in key_events.read() {
         match (key_event.modifiers, key_event.code) {
@@ -60,7 +68,13 @@ fn handle_key_input_system(
             (m, KeyCode::Backspace) if m == KeyModifiers::NONE => {
                 text.pop();
             }
-            _ => {}
+            _ => continue,
         }
+
+        should_render = true;
+    }
+
+    if should_render {
+        render_events.write_default();
     }
 }
