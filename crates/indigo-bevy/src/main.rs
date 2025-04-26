88b0f505fccdc62cc2c6952d5b94fbbcdd8c9cf9
Evan Relf <evan@evanrelf.com>
2025-04-25 21:23:26 -0700

    Add minimal plugins
    Most notably includes a task pool and a schedule runner. Schedules like
    `Update` now run repeatedly forever.

--- crates/indigo-bevy/src/main.rs
+++ crates/indigo-bevy/src/main.rs
@@ -1,9 +1,21 @@
-use bevy::prelude::*;
+use bevy::{app::ScheduleRunnerPlugin, prelude::*};
+use std::time::Duration;
 
 fn main() {
-    App::new().add_systems(Startup, hello_world).run();
+    App::new()
+        .add_plugins(MinimalPlugins.set({
+            let min_wait = Duration::from_secs_f64(1.0 / 60.0);
+            ScheduleRunnerPlugin::run_loop(min_wait)
+        }))
+        .add_systems(Startup, hello_world)
+        .add_systems(Update, tick)
+        .run();
 }
 
 fn hello_world() {
     println!("Hello, world!");
+}
+
+fn tick() {
+    println!("Tick");
 }
