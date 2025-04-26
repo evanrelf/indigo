91ad542fc052d1d1a2c49e463090f749c06025fc
Evan Relf <evan@evanrelf.com>
2025-04-25 22:04:35 -0700

    Add `clap`

--- crates/indigo-bevy/src/main.rs
+++ crates/indigo-bevy/src/main.rs
@@ -1,7 +1,13 @@
 use bevy::{app::ScheduleRunnerPlugin, prelude::*};
+use clap::Parser as _;
 use std::time::Duration;
 
+#[derive(clap::Parser)]
+struct Args {}
+
 fn main() {
+    let _args = Args::parse();
+
     App::new()
         .add_plugins(MinimalPlugins.set({
             let min_wait = Duration::from_secs_f64(1.0 / 60.0);
