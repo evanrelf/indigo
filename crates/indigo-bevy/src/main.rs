c1f462c698ca66f19f3524b2592343b8e63bb6b8
Evan Relf <evan@evanrelf.com>
2025-04-25 21:39:46 -0700

    Drop `bevy_log` and console-related systems
    We'll be rendering to the terminal with `ratatui` soon!

--- crates/indigo-bevy/src/main.rs
+++ crates/indigo-bevy/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*};
+use bevy::{app::ScheduleRunnerPlugin, prelude::*};
 use std::time::Duration;
 
 fn main() {
@@ -7,16 +7,5 @@ fn main() {
             let min_wait = Duration::from_secs_f64(1.0 / 60.0);
             ScheduleRunnerPlugin::run_loop(min_wait)
         }))
-        .add_plugins(LogPlugin::default())
-        .add_systems(Startup, hello_world)
-        .add_systems(Update, tick)
         .run();
-}
-
-fn hello_world() {
-    info!("Hello, world!");
-}
-
-fn tick() {
-    info!("Tick");
 }
