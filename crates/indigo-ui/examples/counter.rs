98bbc902c578e19de81b6fb7ce8837c063b69fa3
Evan Relf <evan@evanrelf.com>
2025-10-26 21:41:52 -0700

    Abandon `indigo-ui` experiment

--- crates/indigo-ui/examples/counter.rs
+++
@@ -1,9 +0,0 @@
-use indigo_ui::{App, View, button};
-
-fn example(count: &mut i32) -> impl View<i32> + use<> {
-    button(format!("click me! count = {count}"), |count| *count += 1)
-}
-
-fn main() {
-    App::new(0, example).run();
-}
