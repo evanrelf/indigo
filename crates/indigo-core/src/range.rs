e8b37281b6658588dbbc1343d2902805b193610f
Evan Relf <evan@evanrelf.com>
2025-11-17 18:34:39 -0800

    Rearrange some seek-related range methods

--- crates/indigo-core/src/range.rs
+++ crates/indigo-core/src/range.rs
@@ -256,43 +256,15 @@ impl<W: WrapMut> RangeView<'_, W> {
         self.update_goal_column();
     }
 
-    pub fn extend_until_next_byte(&mut self, byte: u8) {
-        self.head_mut().move_to_next_byte(byte);
-        self.update_goal_column();
-    }
-
-    pub fn move_until_prev_byte(&mut self, byte: u8) {
-        self.reduce();
-        self.head_mut().move_to_prev_byte(byte);
-        self.update_goal_column();
-    }
-
-    pub fn move_onto_prev_byte(&mut self, byte: u8) {
-        self.reduce();
-        if self.head_mut().move_to_prev_byte(byte) {
-            self.extend_left(1);
-        }
-        self.update_goal_column();
-    }
-
-    pub fn move_until_next_byte(&mut self, byte: u8) {
-        self.reduce();
-        self.head_mut().move_to_next_byte(byte);
-        self.update_goal_column();
-    }
-
-    pub fn move_onto_next_byte(&mut self, byte: u8) {
-        self.reduce();
-        if self.head_mut().move_to_next_byte(byte) {
-            self.extend_right(1);
-        }
-        self.update_goal_column();
-    }
-
     pub fn extend_onto_prev_byte(&mut self, byte: u8) {
         if self.head_mut().move_to_prev_byte(byte) && self.is_backward() {
             self.extend_left(1);
         }
+        self.update_goal_column();
+    }
+
+    pub fn extend_until_next_byte(&mut self, byte: u8) {
+        self.head_mut().move_to_next_byte(byte);
         self.update_goal_column();
     }
 
@@ -327,6 +299,34 @@ impl<W: WrapMut> RangeView<'_, W> {
     pub fn move_down(&mut self, count: usize) {
         self.extend_down(count);
         self.reduce();
+    }
+
+    pub fn move_until_prev_byte(&mut self, byte: u8) {
+        self.reduce();
+        self.head_mut().move_to_prev_byte(byte);
+        self.update_goal_column();
+    }
+
+    pub fn move_onto_prev_byte(&mut self, byte: u8) {
+        self.reduce();
+        if self.head_mut().move_to_prev_byte(byte) {
+            self.extend_left(1);
+        }
+        self.update_goal_column();
+    }
+
+    pub fn move_until_next_byte(&mut self, byte: u8) {
+        self.reduce();
+        self.head_mut().move_to_next_byte(byte);
+        self.update_goal_column();
+    }
+
+    pub fn move_onto_next_byte(&mut self, byte: u8) {
+        self.reduce();
+        if self.head_mut().move_to_next_byte(byte) {
+            self.extend_right(1);
+        }
+        self.update_goal_column();
     }
 
     pub fn flip(&mut self) {
