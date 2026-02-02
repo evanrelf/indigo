50445a8e81a0e01f9913c2a0f7792398ddd614f0
Evan Relf <evan@evanrelf.com>
2026-02-01 18:53:03 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#![expect(dead_code)]
+
 use anyhow::Context;
 use clap::Parser as _;
 use jiff::Timestamp;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::{env, sync::LazyLock};
 
 #[derive(clap::Parser)]
@@ -11,9 +13,37 @@ struct Args {}
 async fn main() -> anyhow::Result<()> {
     let _args = Args::parse();
 
-    let model_info = claude::get_model("claude-sonnet-4-20250514").await?;
+    let model_id = "claude-haiku-4-5";
 
-    println!("{model_info:?}");
+    // let model_info = claude::models_retrieve(model_id).await?;
+
+    // println!("model info:\n{model_info:?}\n");
+
+    let messages_create_params = claude::MessageCreateParams {
+        max_tokens: 1024,
+        messages: vec![
+            claude::MessageParam {
+                content: String::from("Hello, Claude!"),
+                role: String::from("user"),
+            },
+            claude::MessageParam {
+                content: String::from("Hello, Evan!"),
+                role: String::from("assistant"),
+            },
+        ],
+        model: String::from(model_id),
+        // system: None,
+        // temperature: None,
+    };
+
+    println!(
+        "request:\n{}\n",
+        serde_json::to_string(&messages_create_params)?
+    );
+
+    let message = claude::messages_create(messages_create_params).await?;
+
+    println!("response:\n{message:?}\n");
 
     Ok(())
 }
@@ -36,7 +66,6 @@ mod claude {
         LazyLock::force(&CLIENT)
     }
 
-    /// <https://platform.claude.com/docs/en/api/models/retrieve>
     #[derive(Debug, Deserialize)]
     pub struct ModelInfo {
         pub id: String,
@@ -47,15 +76,91 @@ mod claude {
     }
 
     /// <https://platform.claude.com/docs/en/api/models/retrieve>
-    pub async fn get_model(model_id: &str) -> anyhow::Result<ModelInfo> {
+    pub async fn models_retrieve(model_id: &str) -> anyhow::Result<ModelInfo> {
         let response = client()
             .get(format!("{BASE_URL}/v1/models/{model_id}"))
             .header("anthropic-version", "2023-06-01")
             .header("X-Api-Key", api_key()?)
             .send()
             .await?;
+        if !response.status().is_success() {
+            let status = response.status();
+            let text = response.text().await?;
+            anyhow::bail!("failed\nstatus: {status}\ntext: {text}");
+        }
         let model_info: ModelInfo = response.json().await?;
         assert_eq!(model_info.r#type, "model");
         Ok(model_info)
+    }
+
+    #[derive(Debug, Serialize)]
+    pub struct MessageParam {
+        pub content: String,
+        /// One of "user" or "assistant"
+        pub role: String,
+    }
+
+    // #[derive(Serialize)]
+    // pub struct Tool {}
+
+    #[derive(Debug, Serialize)]
+    pub struct MessageCreateParams {
+        pub max_tokens: usize,
+        pub messages: Vec<MessageParam>,
+        pub model: String,
+        // pub system: Option<String>,
+        // pub temperature: Option<f32>,
+        // pub tools: Vec<Tool>,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct TextBlock {
+        // citations: Vec<()>,
+        text: String,
+        /// Always "text"
+        r#type: String,
+    }
+
+    // #[derive(Debug, Deserialize)]
+    // pub struct ToolUseBlock {
+    //     // id: String,
+    //     // input: (),
+    //     // name: String,
+    //     /// Always "tool_use"
+    //     #[expect(clippy::doc_markdown)]
+    //     r#type: String,
+    // }
+
+    #[derive(Debug, Deserialize)]
+    pub enum ContentBlock {
+        TextBlock(TextBlock),
+        // ToolUseBlock(ToolUseBlock),
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Message {
+        pub id: String,
+        pub content: Vec<ContentBlock>,
+        pub model: String,
+        /// Always "assistant"
+        pub role: String,
+    }
+
+    /// <https://platform.claude.com/docs/en/api/messages/create>
+    pub async fn messages_create(params: MessageCreateParams) -> anyhow::Result<Message> {
+        let response = client()
+            .post(format!("{BASE_URL}/v1/messages"))
+            .header("anthropic-version", "2023-06-01")
+            .header("X-Api-Key", api_key()?)
+            .json(&params)
+            .send()
+            .await?;
+        if !response.status().is_success() {
+            let status = response.status();
+            let text = response.text().await?;
+            anyhow::bail!("failed\nstatus: {status}\ntext: {text}");
+        }
+        let message: Message = response.json().await?;
+        Ok(message)
     }
 }
