19246151f00f448c2274b3e039ac86c9191822d0
Evan Relf <evan@evanrelf.com>
2026-03-07 15:46:55 -0800

    Add structs for query/set/reset modes

--- crates/illuso-tui/src/lib.rs
+++ crates/illuso-tui/src/lib.rs
@@ -104,6 +104,32 @@ pub const CLEAR_TO_END_OF_SCREEN: &str = "\x1b[J";
 pub const CLEAR_TO_START_OF_SCREEN: &str = "\x1b[1J";
 pub const CLEAR_SCREEN: &str = "\x1b[2J";
 
+// Modes
+
+pub struct QueryMode(pub u16);
+
+impl Display for QueryMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "\x1b[?{}$p", self.0)
+    }
+}
+
+pub struct SetMode(pub u16);
+
+impl Display for SetMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "\x1b[?{}h", self.0)
+    }
+}
+
+pub struct ResetMode(pub u16);
+
+impl Display for ResetMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "\x1b[?{}l", self.0)
+    }
+}
+
 // In-band resize
 // https://gist.github.com/rockorager/e695fb2924d36b2bcf1fff4a3704bd83
 
