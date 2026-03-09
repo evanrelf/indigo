1fbde1b9218225a45282bbef263157ed173da55d
Evan Relf <evan@evanrelf.com>
2026-03-08 20:18:27 -0700

    Update resize example to use new code

--- crates/illuso-term/examples/resize.rs
+++ crates/illuso-term/examples/resize.rs
@@ -1,24 +1,36 @@
-use illuso_term::{escape, old::*};
+use illuso_term::{escape, event::Event, reader::Reader, tty::Tty};
 use std::{
     io::{self, Write as _},
     process,
 };
 
 fn main() -> io::Result<()> {
-    enable_raw_mode()?;
+    let mut tty = Tty::init()?;
+    let mut reader = Reader::new();
 
-    let mut parser = Parser::new();
+    write!(tty, "{}", escape::IN_BAND_RESIZE_QUERY)?;
+    tty.flush()?;
 
-    if !enable_in_band_resize(&mut parser)? {
-        disable_raw_mode()?;
-        panic!("failed to enable in-band resize (mode 2048)");
+    match reader.read_event(&mut tty)? {
+        Event::Decrpm { mode: 2048, value } => match value {
+            1 | 3 => {
+                // Already set (1) or permanently set (3), no action needed
+            }
+            2 => {
+                // Reset but settable, enable it
+                write!(tty, "{}", escape::IN_BAND_RESIZE_SET)?;
+                tty.flush()?;
+            }
+            _ => panic!("failed to enable in-band resize (mode 2048)"),
+        },
+        event => panic!("unexpected terminal input event: {event:?}"),
     }
 
     let mut stdout = io::stdout().lock();
     let pid = process::id();
 
     loop {
-        if let Some(Event::Resize { height, width, .. }) = read_event(&mut parser, None)? {
+        if let Event::Resize { height, width } = reader.read_event(&mut tty)? {
             write!(
                 stdout,
                 "{}{}pid:{pid},height:{height},width:{width}",
