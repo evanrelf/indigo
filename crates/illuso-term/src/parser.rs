0529fb04bd161e5287ba7f5e929b2a22c8275650
Evan Relf <evan@evanrelf.com>
2026-03-08 17:14:18 -0700

    Revert "Use `seq!` macro"
    This reverts commit f2a5d5ae67c1c830eb2854f9f98d12190f4688a6.

--- crates/illuso-term/src/parser.rs
+++ crates/illuso-term/src/parser.rs
@@ -2,7 +2,7 @@ use crate::event::Event;
 use std::str;
 use winnow::{
     ascii::digit1,
-    combinator::{alt, cut_err, opt, peek, repeat, separated, seq},
+    combinator::{alt, cut_err, opt, peek, repeat, separated},
     prelude::*,
     token::one_of,
 };
@@ -45,18 +45,16 @@ fn unknown_csi(input: &mut &[u8]) -> winnow::ModalResult<Event> {
 
 // CSI 48 ; height ; width ; height_pixels ; width_pixels t
 fn in_band_resize(input: &mut &[u8]) -> winnow::ModalResult<Event> {
-    seq!(Event::Resize {
-        _: "\x1b[48;",
-        height: u16,
-        _: ";",
-        width: u16,
-        _: ";",
-        _: u16,
-        _: ";",
-        _: u16,
-        _: "t",
-    })
-    .parse_next(input)
+    "\x1b[48;".void().parse_next(input)?;
+    let height = u16.parse_next(input)?;
+    ";".void().parse_next(input)?;
+    let width = u16.parse_next(input)?;
+    ";".void().parse_next(input)?;
+    let _height_pixels = u16.parse_next(input)?;
+    ";".void().parse_next(input)?;
+    let _width_pixels = u16.parse_next(input)?;
+    "t".void().parse_next(input)?;
+    Ok(Event::Resize { height, width })
 }
 
 fn u8(input: &mut &[u8]) -> winnow::ModalResult<u8> {
