8ed7a7f1396c0f28482bb86c9da7b93cfc3a9d7f
Evan Relf <evan@evanrelf.com>
2026-03-10 18:49:04 -0700

    Rename `*Mode` to `Mode*`

--- crates/illuso-term/src/lib.rs
+++ crates/illuso-term/src/lib.rs
@@ -20,25 +20,25 @@ pub const CLEAR_SCREEN: &str = "\x1b[2J";
 
 // Modes
 
-pub struct QueryMode(pub u16);
+pub struct ModeQuery(pub u16);
 
-impl Display for QueryMode {
+impl Display for ModeQuery {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "\x1b[?{}$p", self.0)
     }
 }
 
-pub struct SetMode(pub u16);
+pub struct ModeSet(pub u16);
 
-impl Display for SetMode {
+impl Display for ModeSet {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "\x1b[?{}h", self.0)
     }
 }
 
-pub struct ResetMode(pub u16);
+pub struct ModeReset(pub u16);
 
-impl Display for ResetMode {
+impl Display for ModeReset {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "\x1b[?{}l", self.0)
     }
@@ -52,22 +52,22 @@ pub const DA1_QUERY: &str = "\x1b[c";
 // Bracketed paste
 // https://en.wikipedia.org/wiki/Bracketed-paste
 
-pub const BRACKETED_PASTE_QUERY: QueryMode = QueryMode(2004);
-pub const BRACKETED_PASTE_SET: SetMode = SetMode(2004);
-pub const BRACKETED_PASTE_RESET: ResetMode = ResetMode(2004);
+pub const BRACKETED_PASTE_QUERY: ModeQuery = ModeQuery(2004);
+pub const BRACKETED_PASTE_SET: ModeSet = ModeSet(2004);
+pub const BRACKETED_PASTE_RESET: ModeReset = ModeReset(2004);
 pub const BRACKETED_PASTE_BEGIN: &str = "\x1b[200~";
 pub const BRACKETED_PASTE_END: &str = "\x1b[201~";
 
 // In-band resize
 // https://gist.github.com/rockorager/e695fb2924d36b2bcf1fff4a3704bd83
 
-pub const IN_BAND_RESIZE_QUERY: QueryMode = QueryMode(2048);
-pub const IN_BAND_RESIZE_SET: SetMode = SetMode(2048);
-pub const IN_BAND_RESIZE_RESET: ResetMode = ResetMode(2048);
+pub const IN_BAND_RESIZE_QUERY: ModeQuery = ModeQuery(2048);
+pub const IN_BAND_RESIZE_SET: ModeSet = ModeSet(2048);
+pub const IN_BAND_RESIZE_RESET: ModeReset = ModeReset(2048);
 
 // Synchronized output
 // https://github.com/contour-terminal/vt-extensions/blob/master/synchronized-output.md
 
-pub const SYNC_UPDATE_QUERY: QueryMode = QueryMode(2026);
-pub const SYNC_UPDATE_SET: SetMode = SetMode(2026);
-pub const SYNC_UPDATE_RESET: ResetMode = ResetMode(2026);
+pub const SYNC_UPDATE_QUERY: ModeQuery = ModeQuery(2026);
+pub const SYNC_UPDATE_SET: ModeSet = ModeSet(2026);
+pub const SYNC_UPDATE_RESET: ModeReset = ModeReset(2026);
