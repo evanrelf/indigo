179689b35f1e4ea63534b2062faece9ac2a2941a
Evan Relf <evan@evanrelf.com>
2026-03-25 21:48:35 -0700

    Become a flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    crane.url = "github:ipetkov/crane";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem = { pkgs, system, ... }: {
+        packages.default =
+          let
+            crane = inputs.crane.mkLib pkgs;
+
+            commonArgs = {
+              pname = "indigo";
+              version = inputs.self.shortRev or inputs.self.dirtyShortRev;
+              src = crane.cleanCargoSource ./.;
+              strictDeps = true;
+            };
+
+            cargoArtifacts = crane.buildDepsOnly commonArgs;
+
+          in
+          crane.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+            cargoExtraArgs = "--bin indigo";
+          });
+      };
+    };
+}
