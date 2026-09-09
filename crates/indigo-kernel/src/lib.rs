b0919a49d23652dd6f36b169511c7ab733bf3596
Evan Relf <evan@evanrelf.com>
2026-09-09 19:29:09 -0400

    Add comment about kernel module stability

--- crates/indigo-kernel/src/lib.rs
+++ crates/indigo-kernel/src/lib.rs
@@ -1,6 +1,9 @@
+// Finished, used by `indigo-core`
+pub mod edit;
+pub mod grapheme;
+
+// Under construction
 pub mod crdt_vibed_length;
 pub mod crdt_vibed_not_length;
 pub mod document;
-pub mod edit;
-pub mod grapheme;
 pub mod merge;
