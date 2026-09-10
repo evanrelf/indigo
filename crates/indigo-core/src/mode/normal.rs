23b6259f9da4114d80b281244f12fb313da7d351
Evan Relf <evan@evanrelf.com>
2026-09-09 23:02:05 -0400

    Make regex multi-line
    Matches Kakoune's behavior. Here's an example of the mismatch, which was
    resolved by this change:
    
    ```
    $ head LICENSE | ./bin/run-cli '%s^<ret>ixxx'
    xxxMozilla Public License Version 2.0
    ==================================
    
    1. Definitions
    --------------
    
    1.1. "Contributor"
        means each individual or legal entity that creates, contributes to
        the creation of, or owns Covered Software.
    ```
    
    ```
    $ head LICENSE | kak -n -f 'gk%s^<ret>ixxx'
    xxxMozilla Public License Version 2.0
    xxx==================================
    xxx
    xxx1. Definitions
    xxx--------------
    xxx
    xxx1.1. "Contributor"
    xxx    means each individual or legal entity that creates, contributes to
    xxx    the creation of, or owns Covered Software.
    xxx
    ```

--- crates/indigo-core/src/mode/normal.rs
+++ crates/indigo-core/src/mode/normal.rs
@@ -462,7 +462,10 @@ fn split_into_lines(editor: &mut Editor) {
 
 fn select_regex(editor: &mut Editor) {
     prompt::enter(editor, "select", |editor, regex_str| {
-        if let Ok(regex) = Regex::new(regex_str) {
+        let regex = Regex::builder()
+            .syntax(regex_cursor::regex_automata::util::syntax::Config::new().multi_line(true))
+            .build(regex_str);
+        if let Ok(regex) = regex {
             let matched = editor
                 .focused_window_mut()
                 .selection_mut()
